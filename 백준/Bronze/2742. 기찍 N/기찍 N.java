f0363d24d1ec8e10302a830522a72e9ca86f6d24
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-06-19 22:56:02 +0900

    [Bronze III] Title: 기찍 N, Time: 1400 ms, Memory: 33804 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main{
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = scanner.nextInt();
+		
+		for(int i=num; i>0; i--) {
+			System.out.println(i);
+		}
+		
+	}
+}
