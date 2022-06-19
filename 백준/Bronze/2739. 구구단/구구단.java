477db25740d6b9c56c8568a13de88c6659ce9790
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-06-19 22:36:47 +0900

    [Bronze V] Title: 구구단, Time: 108 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int N = scanner.nextInt();
+		
+		for(int i=1; i<10; i++) {
+			System.out.println(N+" * "+i+" = "+ N*i);
+		}
+		
+ 	}
+}
+
+
