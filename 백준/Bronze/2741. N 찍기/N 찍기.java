24a06ab22868bc93e1182501d52c913eac7ca110
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-06-19 22:54:35 +0900

    [Bronze V] Title: N 찍기, Time: 1524 ms, Memory: 32300 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = scanner.nextInt();
+		
+		for(int i=1; i<=num; i++) {
+			System.out.println(i);
+		}
+		
+	}
+}
+
+
