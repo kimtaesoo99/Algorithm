760d6158fb11ae8822b0e039699dd0a3708fc94d
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-06-19 23:14:27 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 196 ms, Memory: 13948 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		for(int i=0; i<N; i++) {
+		for(int j=0; j<=i; j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+
+		}
+		
+	}
+}
