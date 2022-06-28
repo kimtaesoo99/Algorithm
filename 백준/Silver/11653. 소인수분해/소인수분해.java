7ec13284e8bee61113eefc9c4bacd12f414556d8
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-06-28 21:31:57 +0900

    [Silver V] Title: 소인수분해, Time: 240 ms, Memory: 17832 KB -BaekjoonHub

---
+++ 백준/Silver/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+    	Scanner sc = new Scanner(System.in);
+    	int N = sc.nextInt();
+    	
+    	for(int i=2; i<=N;i++) {
+    		if(N%i==0) {
+    			System.out.println(i);
+    			N/=i;
+    			--i;
+    		}
+    	}
+}
+}
