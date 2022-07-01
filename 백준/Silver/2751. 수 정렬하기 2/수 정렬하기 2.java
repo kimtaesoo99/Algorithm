7c3aa19e0437b8e3b5f499b1172d02224be8816f
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-07-01 23:30:15 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2880 ms, Memory: 334228 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder(); 
+        int N = sc.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>(); 
+
+        for (int i = 0; i < N; i++) {
+            list.add(sc.nextInt());
+        }
+
+        Collections.sort(list);
+
+        for (Integer c : list) {
+            sb.append(c).append("\n");
+        }
+
+        System.out.println(sb);
+        sc.close();
+    }
+}
