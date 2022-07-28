cd206e631a2f136fbe5fc2d135fec38755d596a6
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-07-29 00:29:16 +0900

    [Silver IV] Title: 카드2, Time: 264 ms, Memory: 48924 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Queue q = new LinkedList();
+        int N =sc.nextInt();
+
+        for (int i=1;i<=N; i++){
+            q.offer(i);
+        }
+
+        while(true){
+            if(N==1)break;
+            q.poll();
+            N--;
+            if(N==1)break;
+            q.offer(q.poll());
+
+        }
+        System.out.println(q.peek());
+    }
+}
