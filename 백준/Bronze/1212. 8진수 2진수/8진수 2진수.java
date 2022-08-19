cb4af592283070b02d9bf4822077b061bb84e342
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-08-20 00:53:45 +0900

    [Bronze II] Title: 8진수 2진수, Time: 624 ms, Memory: 52836 KB -BaekjoonHub

---
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            String a = Integer.toBinaryString(s.charAt(i)-'0');
+            if(a.length()==2 && i!=0) a="0"+a;
+            else if(a.length()==1 && i!=0) a="00"+a;
+
+            sb.append(a);
+        }
+
+        System.out.println(sb);
+    }
+}
