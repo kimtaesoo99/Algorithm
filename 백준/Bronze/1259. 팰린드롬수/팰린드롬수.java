bd4d81b39aa5321da129e5497a62593522f44f7a
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-08-23 00:04:44 +0900

    [Bronze I] Title: 팰린드롬수, Time: 116 ms, Memory: 14016 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String str = br.readLine();
+            boolean check = true;
+            if (str.equals("0")) break;
+            for (int i = 0; i < str.length() / 2; i++) {
+                if (str.charAt(i) != str.charAt(str.length() - 1 - i)) check = false;
+            }
+            if (check) System.out.println("yes");
+            else System.out.println("no");
+        }
+    }
+}
