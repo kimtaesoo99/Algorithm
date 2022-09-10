26a4045918a92793055cfdad91cd0904fe6a394a
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-09-10 14:04:15 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 156 ms, Memory: 16216 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -1,40 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) throws IOException{
+
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
-        //알파벳 카운트
-        int[] alpha = new int[26];
-        for(int i=0; i<line.length(); i++) {
-            int idx = line.charAt(i)-'A';
-            alpha[idx]++;
+        StringBuilder sb = new StringBuilder();
+
+        String str = br.readLine();
+        int[] arr = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+
+            int index = str.charAt(i) - 'A';
+            arr[index]++;
+        }
+        int odd = 0;
+        int num = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                odd++;
+                num = i;
+            }
+            if (odd >= 2) {
+                System.out.println("I'm Sorry Hansoo");
+                return;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i] / 2; j++) {
+                sb.append((char)(i+'A'));
+            }
+        }
+        String st = sb.toString();
+        if (odd == 1) {
+            st += (char) (num + 'A');
         }
 
-        int odd =0;
-        for(int i=0; i<alpha.length; i++) {
-            if(alpha[i]%2!=0) odd++;
-        }
-        String result ="";
-        StringBuilder sb = new StringBuilder();
-        if(odd>1) result += "I'm Sorry Hansoo";
-        else {
-            for(int i=0; i<alpha.length; i++) {
-                for(int r=0; r<alpha[i]/2; r++) {
-                    sb.append((char)(i+65));
-                }
-            }
-            result += sb.toString();
-            String end = sb.reverse().toString();
-            sb = new StringBuilder();
-            for(int i=0; i<alpha.length; i++) {
-                if(alpha[i]%2==1) {
-                    sb.append((char)(i+65));
-                }
-            }
-            result +=sb.toString()+end;
-        }
-        System.out.println(result);
+        st += sb.reverse().toString();
+        System.out.println(st);
+
     }
 }
