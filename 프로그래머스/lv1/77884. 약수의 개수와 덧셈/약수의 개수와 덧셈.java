fae515e12b24e6bbbb3c9363f19de6faefb066c8
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-03 11:47:47 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.17 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int solution(int left, int right) {
+        int even=0;
+        int odd=0;
+        for (int i=left;i<=right;i++){
+            int count=0;
+            for (int j=1;j<=i;j++){
+                if (i%j==0)count++;
+            }
+            if (count%2==0)even+=i;
+            else odd+=i;
+        }
+        return even-odd;
+    }
+}
