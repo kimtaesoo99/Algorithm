f02e3b14af353ae6f3edacc508afade571702454
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-14 17:42:26 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.97 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,37 +1,14 @@
-
-
-import java.util.*;
-
 public class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        for (int i =0;i<n;i++){
-            String first = Integer.toBinaryString(arr1[i]);
-            if (first.length()<n){
-                String empty="";
-                for (int k=0;k<n-first.length();k++){
-                    empty += "0";
-                }
-                first = empty+first;
-            }
-            String second = Integer.toBinaryString(arr2[i]);
-            if (second.length()<n){
-                String empty="";
-                for (int k=0;k<n-second.length();k++){
-                    empty += "0";
-                }
-                second = empty+second;
-            }
-            StringBuilder sb = new StringBuilder();
-            for (int j=0; j <n;j++){
-                if (first.charAt(j)=='1'||second.charAt(j)=='1'){
-                    sb.append("#");
-                }
-                else sb.append(" ");
-            }
-            answer[i] = sb.toString();
+        String temp;
+        for (int i=0;i<n;i++){
+            temp = String.format("%16s",Integer.toBinaryString(arr1[i]|arr2[i]));
+            temp = temp.substring(temp.length()-n);
+            temp = temp.replaceAll("1","#");
+            temp = temp.replaceAll("0"," ");
+            answer[i] =temp;
         }
-
         return answer;
     }
 }
