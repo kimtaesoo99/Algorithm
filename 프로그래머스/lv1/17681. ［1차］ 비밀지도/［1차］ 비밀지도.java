54908006015cd8bf5596519bbd9527fc26cf2cb9
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-14 17:17:42 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.99 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,37 @@
+
+
+import java.util.*;
+
+public class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        for (int i =0;i<n;i++){
+            String first = Integer.toBinaryString(arr1[i]);
+            if (first.length()<n){
+                String empty="";
+                for (int k=0;k<n-first.length();k++){
+                    empty += " ";
+                }
+                first = empty+first;
+            }
+            String second = Integer.toBinaryString(arr2[i]);
+            if (second.length()<n){
+                String empty="";
+                for (int k=0;k<n-second.length();k++){
+                    empty += " ";
+                }
+                second = empty+second;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int j=0; j <n;j++){
+                if (first.charAt(j)=='1'||second.charAt(j)=='1'){
+                    sb.append("#");
+                }
+                else sb.append(" ");
+            }
+            answer[i] = sb.toString();
+        }
+
+        return answer;
+    }
+}
