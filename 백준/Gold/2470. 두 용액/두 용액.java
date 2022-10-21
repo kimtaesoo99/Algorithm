d2e9df98e8e7dbc17d80abb4c447756935fc7fff
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-21 17:58:50 +0900

    [Gold V] Title: 두 용액, Time: 388 ms, Memory: 28564 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		int i = 0;
+		int j = arr.length - 1;
+		
+		int gap = Integer.MAX_VALUE;
+		int ans1 = 0;
+		int ans2 = 0;
+		
+		int temp;
+		int sum;
+		while (i < j) {
+			sum = arr[i] + arr[j];
+			temp = Math.abs(sum);
+			if (temp < gap) {
+				gap = temp;
+				ans1 = arr[i];
+				ans2 = arr[j];
+			}
+			if (sum > 0)
+				j--;
+			else
+				i++;
+		}
+		
+		System.out.println(ans1 + " " + ans2);
+	}
+}
