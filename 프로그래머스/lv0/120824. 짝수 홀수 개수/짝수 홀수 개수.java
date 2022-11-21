bfa7b3fc4f81495f60070f43b39939f390c6ff84
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-21 12:06:58 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        for (int i : num_list){
+            if (i%2==0)answer[0]++;
+            else answer[1]++;
+        }
+        return answer;
+    }
+}
