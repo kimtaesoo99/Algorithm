3798a01cc7caaa5e74cf13633935c752292347e4
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-29 11:25:01 +0900

    [level 0] Title: 문자열 계산하기, Time: 1.75 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -1,28 +1,9 @@
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 class Solution {
     public int solution(String my_string) {
-          List<Integer> cost = Arrays.stream(my_string.split(" "))
-                .filter(i -> i.matches("[0-9]+"))
-                .map(i -> Integer.parseInt(i))
-                .collect(Collectors.toList());
-
-        List<String> operator = Arrays.stream(my_string.split(" "))
-                .filter(i -> i.equals("+") || i.equals("-"))
-                .collect(Collectors.toList());
-        
-        int sum = cost.get(0);
-        int count = 1;
-        for (String next : operator){
-            int nextInt = cost.get(count++);
-            if (next.equals("-")){
-                nextInt = (-1)*nextInt;
-            }
-            sum +=nextInt;
-        }
-
-        return sum;
+         return Arrays.stream(my_string.replaceAll("- ","-")
+                .replaceAll("[+] ","").split(" "))
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
