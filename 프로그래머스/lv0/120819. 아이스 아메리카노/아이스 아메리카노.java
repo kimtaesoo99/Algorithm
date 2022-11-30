82a041b34f57d57f92b80a61c8931967d41d506f
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-30 18:54:06 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] solution(int money) {
         int[] answer = new int[2];
         answer[0] = money / 5500;
-        answer[1] = money - 5500 * answer[0];
+        answer[1] = money % 5500;
         return answer;
     }
 }
