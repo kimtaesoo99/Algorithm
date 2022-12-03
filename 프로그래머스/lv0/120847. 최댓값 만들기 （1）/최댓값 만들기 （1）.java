6a201bdb8a6de6476419f880a7449618a13d2f78
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-03 11:11:06 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 2.52 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,11 +1,10 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 class Solution {
     public int solution(int[] numbers) {
-          int max =0;
-        for (int i=0;i<numbers.length-1;i++){
-            for (int j=i+1; j<numbers.length;j++){
-                max = Math.max(max,numbers[i]*numbers[j]);
-            }
-        }
-        return max;
+        numbers = Arrays.stream(numbers).boxed().sorted(Comparator.reverseOrder()).mapToInt(Integer::intValue)
+                .toArray();
+        return numbers[0] * numbers[1];
     }
 }
