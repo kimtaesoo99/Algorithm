1edeb7812501c6f47705541acd1d5366fe829e14
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-06 17:42:27 +0900

    [level 2] Title: 전화번호 목록, Time: 273.48 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,24 +1,15 @@
-
-
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        List<String> list = Arrays.stream(phone_book).collect(Collectors.toList());
-        Collections.sort(list);
-     
-
-        for (int i = 0; i < list.size()-1; i++) {
-           if (list.get(i+1).startsWith(list.get(i))){
-               answer = false;
-               break;
-           }
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length-1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                answer = false;
+                break;
+            }
         }
-
         return answer;
     }
 }
