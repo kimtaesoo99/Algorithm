fd56ccbcdd8c62870f147cadbd712a2b2b471728
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-20 12:00:59 +0900

    [Gold V] Title: 탑, Time: 752 ms, Memory: 86672 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,31 +6,32 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Stack<int[]> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i<n; i++){
+            int top = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int num = Integer.parseInt(br.readLine());
-		
-		Stack<int[]> top = new Stack<>(); //Stack에 int형 배열 저장 가능!!
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		for(int i=0; i<num; i++) {
-			int n = Integer.parseInt(st.nextToken());
-			
-			while(!top.isEmpty()) {
-				if(top.peek()[0] < n) //가장 앞의 탑이 n보다 작으면, 앞으로 어떠한 신호도 수신할 수 없으므로 스택에서 제거해버린다.(가지고 있을 필요가 없음)
-					top.pop();
-				else { //n보다 크다면, 해당 탑의 번호를 출력한다.
-					System.out.print(top.peek()[1] + " ");
-					break;
-				}
-			}
-			
-			if(top.empty()) //탑이 비었다면 0을 출력한다.
-				System.out.print("0 ");
-			top.push(new int[] {n, i+1}); //탑의 높이와 탑의 번호 배열을 저장
-		}
-			
-	}
-
+            while (!stack.isEmpty()){
+                if (stack.peek()[0]<top){
+                    stack.pop();
+                }
+                else {
+                    sb.append(stack.peek()[1]).append(" ");
+                    stack.push(new int[]{top,i+1});
+                    break;
+                }
+            }
+            if (stack.isEmpty()){
+                sb.append("0").append(" ");
+                stack.push(new int[]{top,i+1});
+            }
+        }
+        System.out.println(sb);
+    }
 }
+
+
