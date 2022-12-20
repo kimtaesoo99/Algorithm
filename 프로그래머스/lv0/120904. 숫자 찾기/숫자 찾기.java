05e9e5e1c13b8a4f5a11f79c5a94ef9ee16f3064
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-20 11:11:17 +0900

    [level 0] Title: 숫자 찾기, Time: 7.58 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(int num, int k) {
-        String str = " " + num;
-        int count = -1;
-        for (int i = 1; i <str.length();i++){
-            if (str.charAt(i)=='0'+k)return i;
-        }
-        return count;
+        return (" " + num).indexOf(String.valueOf(k));
     }
 }
