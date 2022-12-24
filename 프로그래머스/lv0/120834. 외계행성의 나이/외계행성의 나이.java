34a0fc150f1bd4d88236831ca84e0493a1c607bb
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-24 17:02:01 +0900

    [level 0] Title: 외계행성의 나이, Time: 2.67 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,9 +1,8 @@
+import java.util.stream.Collectors;
+
 class Solution {
     public String solution(int age) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < String.valueOf(age).length(); i++) {
-            sb.append((char)(Integer.parseInt(String.valueOf(age).charAt(i)+"")+'a'));
-        }
-        return sb.toString();
+        return String.valueOf(age).chars()
+                .mapToObj(o -> String.valueOf((char)(49+o))).collect(Collectors.joining());
     }
 }
