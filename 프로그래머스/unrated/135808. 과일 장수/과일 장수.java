0f860e7018de82ddc984c2e793e18e887d87dfe8
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-30 06:20:47 +0900

    [unrated] Title: 과일 장수, Time: 0.32 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -1,15 +1,11 @@
 import java.util.Arrays;
-import java.util.Collections;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
         int sum = 0;
-        Integer[] arr = Arrays.stream(score).boxed().sorted(Collections.reverseOrder()).toArray(Integer[]::new);
-        for (int i = 0; i < arr.length; i++) {
-            if (i == 0) continue;
-            if ((i+1)% m == 0) {
-                sum += m * arr[i];
-            }
+        Arrays.sort(score);
+        for (int i = score.length; i>=m; i -= m){
+            sum += score[i-m] * m;
         }
         return sum;
     }
