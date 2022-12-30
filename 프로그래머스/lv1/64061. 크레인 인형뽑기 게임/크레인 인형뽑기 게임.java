9e0b460cc4adbcc7df963a0242796b493392c2d5
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-30 23:28:27 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.62 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+public class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        for (int move : moves) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][move - 1] != 0) {
+                    if (stack.peek() == board[j][move - 1]) {
+                        stack.pop();
+                        answer += 2;
+                    } else {
+                        stack.push(board[j][move - 1]);
+                    }
+                    board[j][move - 1] = 0;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
