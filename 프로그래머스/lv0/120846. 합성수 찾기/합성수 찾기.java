0fde9b2971c71224aef9a37db5da2a9733846c15
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-02 12:28:21 +0900

    [level 0] Title: 합성수 찾기, Time: 1.66 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -2,13 +2,8 @@ import java.util.stream.IntStream;
 
 class Solution {
     public long solution(int n) {
-        return IntStream.rangeClosed(1, n).filter(i -> {
-            int count = 0;
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0)
-                    count++;
-            }
-            return 3 <= count;
-        }).count();
+        return IntStream.rangeClosed(1, n)
+                .filter(i -> IntStream.rangeClosed(1,i)
+                        .filter(i2 ->i%i2==0).count()>2).count();
     }
 }
