a7d097b6d09849130ba4e25ca40d983571af16c7
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-05 00:19:07 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.18 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,45 +1,18 @@
-
-import java.util.Objects;
-import java.util.Stack;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
-
 class Solution {
     public String solution(String new_id) {
-        Stack<String> stack = new Stack<>();
-        for (int i = 0; i < new_id.length(); i++) {
-            char c = new_id.charAt(i);
-            //처음 . 금지
-            if (stack.isEmpty()&&c=='.')continue;
-            // . 중복금지
-            if (!stack.isEmpty()){
-                if (Objects.equals(stack.peek(), String.valueOf(c)) && c =='.'){
-                    continue;
-                }
-            }
-            // 대문자 소문자로 변형
-            if (Character.isUpperCase(c)){
-                stack.push(String.valueOf(c).toLowerCase());
-                continue;
-            }
-            // 소문지, 숫자,특수문자 허용
-            if (correctChar(c))stack.push(String.valueOf(c));
+        String id = new_id.toLowerCase();
+        id = id.replaceAll("[^-_.a-z0-9]","");
+        id = id.replaceAll("[.]{2,}",".");
+        id = id.replaceAll("^[.]|[.]$","");
+        if (id.isEmpty())return "aaa";
+        if (id.length()>=16){
+            id = id.substring(0,15);
+            id = id.replaceAll("[.]$","");
         }
-        if (stack.isEmpty())return "aaa";
-        String result = stack.stream().limit(15).collect(Collectors.joining());
-        if (result.endsWith(".")) result = result.substring(0,result.length()-1);
-        if (result.length()==1)return result.repeat(3);
-        if (result.length()==2)return result + result.charAt(1);
+        if (id.length()<=2)
+            while (id.length()<3)
+                id+=id.charAt(id.length()-1);
 
-
-        return result;
-    }
-
-    boolean correctChar(char c){
-        String form = "^[a-z0-9]*$";
-        String check = "-._";
-        if (Pattern.matches(form,String.valueOf(c)))return true;
-        return check.contains(String.valueOf(c));
+        return id;
     }
 }
