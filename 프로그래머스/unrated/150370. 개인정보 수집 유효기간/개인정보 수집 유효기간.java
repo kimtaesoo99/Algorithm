c0fcba57b997ad5b6b3dbcb375ee1365ae42743f
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-08 20:46:43 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 3.73 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -3,7 +3,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 class Solution {
     public List<Integer> solution(String today, String[] terms, String[] privacies) {
         Map<String,Integer> map = new HashMap<>();
@@ -12,16 +11,10 @@ class Solution {
             String[] termInfo = term.split(" ");
             map.put(termInfo[0], Integer.parseInt(termInfo[1]));
         }
-
-        System.out.println(map);
-
-        int index = 1;
-        for (String privacy : privacies) {
-            String[] date = privacy.split("\\.| ");
-            if (isOverDate(today,date,map))result.add(index);
-            index++;
+        for (int i =0; i< privacies.length; i++){
+            String[] date = privacies[i].split("\\.| ");
+            if (isOverDate(today,date,map))result.add(i+1);
         }
-
         return result;
     }
 
@@ -29,11 +22,8 @@ class Solution {
         String[] todayInfo = today.split("\\.");
         int validity = map.get(date[3]);
         int year = Integer.parseInt(todayInfo[0]) - Integer.parseInt(date[0]);
-
         int month = (Integer.parseInt(todayInfo[1])+year*12) - Integer.parseInt(date[1]);
-
         int day = (Integer.parseInt(todayInfo[2])+month*28) - Integer.parseInt(date[2]);
-
         int result = day - (validity*28);
         if (result>=0)return true;
         return false;
