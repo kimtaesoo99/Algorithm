a1668b448f9b1647571a51c9d24445ea5fdba9d8
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-11 12:56:41 +0900

    [level 0] Title: 암호 해독, Time: 2.64 ms, Memory: 68.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -1,9 +1,9 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 class Solution {
     public String solution(String cipher, int code) {
-        StringBuilder sb = new StringBuilder();
-        for (int i =0; i< cipher.length();i++){
-            if ((i+1)%code==0)sb.append(cipher.charAt(i));
-        }
-        return sb.toString();
+        return IntStream.rangeClosed(0,cipher.length()-1)
+                .filter(i -> (i+1)%code ==0).mapToObj(n -> String.valueOf(cipher.charAt(n))).collect(Collectors.joining());
     }
 }
