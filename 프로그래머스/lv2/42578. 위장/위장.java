0c35e6f920e7566f077df7fa22173eac1e60a4b2
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-13 15:17:41 +0900

    [level 2] Title: 위장, Time: 6.39 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.java
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -1,23 +1,11 @@
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import static java.util.stream.Collectors.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, List<String>> map = new HashMap<>();
-        for (String[] clothe : clothes) {
-            List<String> list = map.getOrDefault(clothe[1], new LinkedList<>());
-            list.add(clothe[0]);
-            map.put(clothe[1], list);
-        }
-        int combination = 1;
-        List<Integer> sizes = map.values().stream().map(List::size)
-                .collect(Collectors.toList());
-        for (Integer size : sizes) {
-            combination *=(size+1);
-        }
-        return combination-1;
+        return Arrays.stream(clothes)
+                .collect(groupingBy(p -> p[1], mapping(p -> p[0], counting())))
+                .values()
+                .stream().reduce(1L, (x, y) -> x * (y + 1)).intValue()-1;
     }
 }
