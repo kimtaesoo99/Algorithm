77b8fd8818c357e08b2bd1e6e436b2cde720930e
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-14 13:48:24 +0900

    [level 0] Title: A로 B 만들기, Time: 0.35 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
@@ -1,16 +1,12 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 class Solution {
     public int solution(String before, String after) {
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < before.length(); i++){
-            map.put(before.charAt(i),map.getOrDefault(before.charAt(i),0)+1);
-            map.put(after.charAt(i),map.getOrDefault(after.charAt(i),0)-1);
-        }
-        for (Integer value : map.values()) {
-            if (value!=0)return 0;
-        }
-        return 1;
+        char[] a =  after.toCharArray();
+        char[] b =  before.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return new String(a).equals(new String(b))?1 :0;
     }
 }
