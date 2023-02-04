cd98a31f2706903498bff3967848e3c281abdb1a
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-04 13:17:13 +0900

    [Silver II] Title: 최대 힙, Time: 1008 ms, Memory: 113664 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int N = sc.nextInt();
+        PriorityQueue q = new PriorityQueue(Comparator.reverseOrder());
+        for(int i = 0; i<N; i++){
+            int value = sc.nextInt();
+            if(value==0 && q.isEmpty())sb.append(0).append("\n");
+            else if(value==0 && !q.isEmpty())sb.append(q.poll()).append("\n");
+            else q.offer(value);
+        }
+        System.out.println(sb);
+    }
+}
+
+
