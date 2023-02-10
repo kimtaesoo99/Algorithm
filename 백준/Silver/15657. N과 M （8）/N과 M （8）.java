8a077aec237268ddfdd7cede0eaa67e882f3655a
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-10 23:19:08 +0900

    [Silver III] Title: N과 M (8), Time: 228 ms, Memory: 21908 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] arr;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] num = br.readLine().split(" ");
+        int N = Integer.parseInt(num[0]);
+        int M = Integer.parseInt(num[1]);
+        arr = new int[N];
+        result = new int[M];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        getResult(N, M, 0, -1);
+        System.out.println(sb);
+    }
+
+    public static void getResult(int N, int M, int index, int pre) {
+        if (M == index) {
+            for (int i : result) {
+                sb.append(i + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (arr[i] >= pre) {
+                result[index] = arr[i];
+                getResult(N, M, index + 1, arr[i]);
+            }
+        }
+    }
+}
+
