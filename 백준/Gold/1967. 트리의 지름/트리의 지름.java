a78b4f66ce33d9b71c4484712f66bbf06922a9aa
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-12 23:02:09 +0900

    [Gold IV] Title: 트리의 지름, Time: 2636 ms, Memory: 127548 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -1,59 +1,53 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 
 public class Main {
     static int N;
     static List<Node> nodes[];
-    static boolean visited[];
+    static boolean[] visited;
     static int max;
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        nodes = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++){
-            nodes[i] = new ArrayList<>();
-        }
-        for (int i = 0 ; i < N-1; i++){
+        nodes = new ArrayList[N+1];
+        for(int i = 1; i <= N; i++)nodes[i] = new ArrayList<>();
+
+        for(int i = 1; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int value = Integer.parseInt(st.nextToken());
-            nodes[from].add(new Node(to, value));
-            nodes[to].add(new Node(from, value));
+            nodes[from].add(new Node(to,value));
+            nodes[to].add(new Node(from,value));
         }
 
-        for (int i = 1; i <=N; i++){
-            visited = new boolean[N + 1];
+        for(int i = 1; i <= N; i++){
+            visited = new boolean[N+1];
             visited[i] = true;
             dfs(i,0);
         }
         System.out.println(max);
     }
 
-    public static void dfs(int num, int sum){
-        for (Node node : nodes[num]) {
-            if (!visited[node.link]){
+    public static void dfs(int num, int value){
+        for(Node node : nodes[num]){
+            if(!visited[node.link]){
                 visited[node.link] = true;
-                dfs(node.link, sum + node.value);
+                dfs(node.link, value + node.value);
             }
         }
-        max = Math.max(max, sum);
+        max = Math.max(max,value);
     }
+
 }
 
+class Node{
+    public int link;
+    public int value;
 
-class Node {
-    int link;
-    int value;
-
-    public Node(int link, int value) {
+    public Node(int link, int value){
         this.link = link;
-        this.value = value;
+        this.value =value;
     }
 }
