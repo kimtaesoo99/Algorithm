f2de0170cbb655725355c16a2566dfde7db52775
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-13 14:49:51 +0900

    [Gold V] Title: 노드사이의 거리, Time: 240 ms, Memory: 20212 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
@@ -1,55 +1,60 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    static List<Node>[] list;
-    static int[] cost;
-    static int answer;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+public class Main{
+    static List<Node>[] nodes;
+    static int distance;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] num = br.readLine().split(" ");
+        int N = Integer.parseInt(num[0]);
+        int M = Integer.parseInt(num[1]);
 
-        list = new ArrayList[n+1];
-        cost = new int[n+1];
-        for(int i=1; i<n+1; i++) {
-            list[i] = new ArrayList<>();
+        nodes = new ArrayList[N+1];
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i <=N; i++)nodes[i] = new ArrayList<>();
+        for(int i = 0 ; i < N-1; i++){
+            String[] info = br.readLine().split(" ");
+            int from = Integer.parseInt(info[0]);
+            int to = Integer.parseInt(info[1]);
+            int value = Integer.parseInt(info[2]);
+            nodes[from].add(new Node(to,value));
+            nodes[to].add(new Node(from,value));
         }
 
-        for(int i=0; i<n-1; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            int w = sc.nextInt();
-            list[u].add(new Node(v,w));
-            list[v].add(new Node(u,w));
+        for(int i =0; i < M; i++){
+            distance = 0;
+            visited = new boolean[N+1];
+            String[] findNode = br.readLine().split(" ");
+            visited[Integer.parseInt(findNode[0])] =true;
+            dfs(Integer.parseInt(findNode[0]),Integer.parseInt(findNode[1]),0);
+            sb.append(distance).append("\n");
         }
-        for(int i=0; i<m; i++) {
-            int to = sc.nextInt();
-            int from = sc.nextInt();
-            dfs(to, -1, from, 0);
-            System.out.println(answer);
-        }
+        System.out.println(sb);
     }
 
-    static void dfs(int to, int pa, int from, int cost) {
-        if(to == from) {
-            answer = cost;
+    public static void dfs(int start, int end, int sum){
+        if(start == end){
+            distance = sum;
+            return;
         }
-        for(Node n : list[to]) {
-            if(n.to != pa) {
-                dfs(n.to, to, from, cost+n.cost);
+        for(Node node : nodes[start]){
+            if(!visited[node.link]){
+                visited[node.link] = true;
+                dfs(node.link, end, sum + node.value);
+                visited[node.link] = false;
             }
         }
     }
+}
 
-    static class Node{
-        int to;
-        int cost;
+class Node{
+    public int link;
+    public int value;
 
-        public Node(int to, int cost){
-            this.to = to;
-            this.cost = cost;
-        }
+    public Node(int link, int value){
+        this.link = link;
+        this.value = value;
     }
 }
