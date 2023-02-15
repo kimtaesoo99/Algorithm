89e36442e05eaf201ebea52c80c61486206f7650
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-15 13:51:22 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 148 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,25 +1,25 @@
-import java.util.Scanner;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] arr = new int[N];
-        int[] dp = new int[N];
-
-        for (int i=0; i<N; i++){
-            arr[i] = sc.nextInt();
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N+1];
+        int[] dp = new int[N+1];
+        String[] info = br.readLine().split(" ");
+        for(int i = 1; i <= N; i++){
+            arr[i] = Integer.parseInt(info[i-1]);
         }
-        for (int i=0; i<N; i++){
+
+        int max = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= N; i++){
             dp[i] = 1;
-            for (int j=0; j<i; j++){
-                if (arr[j]<arr[i]&&dp[i]<dp[j]+1){
-                    dp[i]=dp[j]+1;
+            for(int j = 1; j<i; j++){
+                if(arr[i]>arr[j]){
+                    dp[i] = Math.max(dp[i],dp[j] + 1);
                 }
             }
-        }
-        int max = 0;
-        for (int i = 0;i<N; i++){
             max = Math.max(max,dp[i]);
         }
         System.out.println(max);
