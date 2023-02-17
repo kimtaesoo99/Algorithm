e5f9e714db9bdc6d5f68902b3c4cef60af3e9471
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-17 21:58:27 +0900

    [Gold V] Title: 동전 1, Time: 224 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,17 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int k = sc.nextInt();
+
         int[] coin = new int[n+1];
         int[] dp = new int[k+1];
+
         dp[0] = 1;
-        for (int i = 1; i<=n; i++){
+
+        for (int i = 1; i <= n; i++){
             coin[i] = sc.nextInt();
-            for (int j = coin[i]; j<=k; j++){
-                dp[j]+=dp[j - coin[i]];
+            for (int j = coin[i]; j <=k; j++){
+                dp[j] += dp[j - coin[i]];
             }
         }
         System.out.println(dp[k]);
