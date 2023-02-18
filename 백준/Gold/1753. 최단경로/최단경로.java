520cd1be69512bbf95afdb6ca805dfae63f064ff
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-18 14:55:02 +0900

    [Gold IV] Title: 최단경로, Time: 896 ms, Memory: 132288 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -5,37 +5,39 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
 
-public class Main {
+public class Main{
     static List<Node>[] nodes;
-    static boolean[] visited;
+    static boolean[] check;
     static int[] distance;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] vAndE = br.readLine().split(" ");
-        int n = Integer.parseInt(vAndE[0]);
+        int v = Integer.parseInt(vAndE[0]);
         int e = Integer.parseInt(vAndE[1]);
+        nodes = new ArrayList[v+1];
+        check = new boolean[v+1];
+        distance = new int[v+1];
+
         int start = Integer.parseInt(br.readLine());
-        nodes = new ArrayList[n+1];
-        visited = new boolean[n+1];
-        distance = new int[n+1];
-        for (int i = 0; i <=n; i++){
+        for (int i = 0; i <= v; i++){
             nodes[i] = new ArrayList<>();
             distance[i] = Integer.MAX_VALUE;
         }
 
-        for (int i = 0 ; i < e; i++){
+        for (int i = 0; i < e; i++){
             String[] info = br.readLine().split(" ");
-            int from  = Integer.parseInt(info[0]);
-            int to  = Integer.parseInt(info[1]);
-            int value  = Integer.parseInt(info[2]);
+            int from = Integer.parseInt(info[0]);
+            int to = Integer.parseInt(info[1]);
+            int value = Integer.parseInt(info[2]);
             nodes[from].add(new Node(to, value));
         }
-        StringBuilder sb= new StringBuilder();
+
+        StringBuilder sb = new StringBuilder();
         dij(start);
-        for (int i = 1; i <= n; i++){
-            if (distance[i] == Integer.MAX_VALUE)
+        for (int i = 1; i <=v; i++){
+            if (distance[i] == Integer.MAX_VALUE) {
                 sb.append("INF").append("\n");
-            else {
+            } else {
                 sb.append(distance[i]).append("\n");
             }
         }
@@ -49,12 +51,12 @@ public class Main {
 
         while (!q.isEmpty()){
             Node poll = q.poll();
-            visited[poll.end] = true;
-            for (Node now : nodes[poll.end]){
-                if (!visited[now.end]){
-                    if (distance[poll.end] + now.cost < distance[now.end]){
-                        distance[now.end] = distance[poll.end] + now.cost;
-                        q.offer(new Node(now.end, distance[now.end]));
+            check[poll.link] = true;
+            for(Node now : nodes[poll.link]){
+                if (!check[now.link]){
+                    if (distance[poll.link] + now.cost < distance[now.link]){
+                        distance[now.link] = distance[poll.link] + now.cost;
+                        q.offer(new Node(now.link, distance[now.link]));
                     }
                 }
             }
@@ -62,18 +64,17 @@ public class Main {
     }
 }
 
-
 class Node implements Comparable<Node>{
-    public int end;
+    public int link;
     public int cost;
 
-    public Node(int end, int cost){
-        this.end = end;
-        this.cost = cost;
+    @Override
+    public int compareTo(Node o){
+        return this.cost - o.cost;
     }
 
-    @Override
-    public int compareTo(Node o) {
-        return this.cost- o.cost;
+    public Node(int link, int cost) {
+        this.link = link;
+        this.cost = cost;
     }
 }
