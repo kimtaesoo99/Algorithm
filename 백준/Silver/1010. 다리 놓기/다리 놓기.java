71217cc2e54a80cbbe7aefe56060bc52e6493b64
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-19 14:22:50 +0900

    [Silver V] Title: 다리 놓기, Time: 444 ms, Memory: 24096 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,29 +1,24 @@
 import java.util.*;
 
-public class Main {
-	static double N;
-	static double M;
-    public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
-    	int T =sc.nextInt();
-    	
-    	for(int c=1; c<=T;c++) {
-    		 N=sc.nextDouble();
-    		 M=sc.nextDouble();
-    		
-    		System.out.printf("%.0f\n",Combination(M,N));
-    	}
-    	
-    }
-    
-    public static double Combination(double n,double m) {
-    		double a=1,b=1;
-    		for(double i=n;i>n-m;i--) {
-    			a=a*i;
-    		}
-    		for(double i=m; i>0;i--) {
-    			b=b*i;
-    		}
-    		return a/b;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        for (int i = 1; i <= test; i++){
+            double n = sc.nextDouble();
+            double m = sc.nextDouble();
+
+            double a = 1;
+            double b = 1;
+
+            for (double j = m; j> m-n; j--){
+                a = a*j;
+            }
+            for (double j = n; j> 0; j--){
+                b = b*j;
+            }
+            double result = a/b;
+            System.out.printf("%.0f \n",result);
+        }
     }
 }
