96a4c134c1478f1b860477a3c96946951ed09ed2
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-20 18:51:21 +0900

    [Silver I] Title: 회의실 배정, Time: 680 ms, Memory: 52444 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,52 +1,41 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        List<Conference> list = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            Conference conference = new Conference(sc.nextInt(), sc.nextInt());
-            list.add(conference);
+public class Main{
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<Time> time = new ArrayList<>();
+        for(int i = 0; i< n;i++){
+            String[] info = br.readLine().split(" ");
+            time.add(new Time(Integer.parseInt(info[0]), Integer.parseInt(info[1])));
         }
-        list.sort(new Comparator<Conference>() {
-            @Override
-            public int compare(Conference o1, Conference o2) {
-                if (o1.getEnd() != o2.getEnd()) {
-                    return o1.getEnd() - o2.getEnd();
-                } else {
-                    return o1.getStart() - o2.getStart();
-                }
-            }
-        });
+        Collections.sort(time);
         int count = 0;
-        int endTime = -1;
-        for (int i = 0; i < N; i++){
-            if (list.get(i).getStart()>=endTime) {
-                endTime = list.get(i).getEnd();
+        int end = 0;
+        for(int i = 0; i < time.size(); i++){
+            if(end <= time.get(i).start){
                 count++;
+                end = time.get(i).end;
             }
         }
         System.out.println(count);
     }
 }
 
-class Conference {
-    private int start;
-    private int end;
+class Time implements Comparable<Time>{
+    int start;
+    int end;
 
-    public int getStart() {
-        return start;
+    @Override
+    public int compareTo(Time t){
+        if(this.end == t.end)return this.start - t.start;
+        return this.end - t.end;
     }
 
-    public int getEnd() {
-        return end;
-    }
-
-    public Conference(int start, int end) {
+    public Time(int start, int end){
         this.start = start;
         this.end = end;
     }
