32800ac3d72cfae38665cb00d8d2eddbb50cb7a3
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-20 14:20:29 +0900

    [Silver I] Title: 카드 구매하기, Time: 148 ms, Memory: 14808 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -1,19 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-import java.util.Scanner;
+public class Main{
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+        int[] arr = new int[n+1];
 
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] dp = new int[N + 1];
-        int[] cards = new int[N + 1];
-
-        for (int i = 1; i <=N; i++){
-            cards[i] = sc.nextInt();
+        String[] info = br.readLine().split(" ");
+        for (int i = 1; i <= n; i++){
+            arr[i] = Integer.parseInt(info[i-1]);
             for (int j = 1; j<=i; j++){
-                dp[i] = Math.max(dp[i], dp[i - j] + cards[j]);
+                dp[i] = Math.max(dp[i], dp[i-j] + arr[j]);
             }
         }
-        System.out.println(dp[N]);
+        System.out.println(dp[n]);
     }
 }
