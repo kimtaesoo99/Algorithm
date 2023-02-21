88aff2998cf10fb48ee57bcceb9ae4c3535b1fbb
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-21 15:19:49 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.16 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 53.7 MB, 시간: 4.09 ms
+메모리: 54 MB, 시간: 7.16 ms
 
 ### 구분
 
--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,38 +1,34 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
+import java.util.*;
 class Solution {
-    int[][] result;
-    int[] dy = {1,0,-1,0};
-    int[] dx = {0,1,0,-1};
+    static int[][] result;
     public int solution(int[][] maps) {
         result = new int[maps.length][maps[0].length];
-        for (int i = 0; i < maps.length; i++){
-            for (int j = 0; j<maps[0].length; j++){
-                result[i][j] = -1;
-            }
-        }
-        bfs(0,0,maps);
-
-        return result[maps.length-1][maps[0].length-1];
+        bfs(0,0, maps);
+        return result[maps.length-1][maps[0].length-1]!=0?result[maps.length-1][maps[0].length-1]:-1;
     }
-    void bfs(int y, int x ,int[][] maps){
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{y,x});
+    
+    public void bfs(int y, int x, int[][] maps){
+        Queue<int[]> q = new LinkedList<>();
         result[y][x] = 1;
-
-        while (!queue.isEmpty()){
-            int[] move = queue.poll();
-            for (int i = 0; i < 4; i++){
-                int moveY = move[0] + dy[i];
-                int moveX = move[1] + dx[i];
-                if (0<= moveX && moveX < maps[0].length && 0<= moveY && moveY <maps.length){
-                    if (maps[moveY][moveX] == 1 && result[moveY][moveX]==-1){
-                        result[moveY][moveX] = result[move[0]][move[1]] + 1;
-                        queue.add(new int[]{moveY, moveX});
+        q.offer(new int[]{y,x});
+        
+        int[] dy = {1,0,-1,0};
+        int[] dx = {0,1,0,-1};
+        
+        while(!q.isEmpty()){
+            int[] poll = q.poll();
+            for(int i = 0; i < 4; i++){
+                int moveY = poll[0] + dy[i];
+                int moveX = poll[1] + dx[i];
+                
+                if(0<=moveY && moveY < maps.length && 0<= moveX && moveX <maps[0].length){
+                    if(maps[moveY][moveX]==1 && result[moveY][moveX] ==0){
+                        result[moveY][moveX] = result[poll[0]][poll[1]] +1;
+                        q.offer(new int[]{moveY,moveX});
                     }
                 }
             }
         }
+        
     }
 }
