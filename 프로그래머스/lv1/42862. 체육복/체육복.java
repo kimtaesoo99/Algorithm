843ccdb407da72e8b3c78f51acdc09f42805fe1a
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-22 23:02:03 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -1,29 +1,28 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int student = n - lost.length;
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] == reserve[j]) {
-                    lost[i] = -1;
-                    reserve[j] = -1;
-                    student++;
-                    break;
+        int answer = n;
+        int[] student = new int[n];
+        for(int l : lost){
+            student[l-1]--;
+        }
+        for(int r : reserve){
+            student[r-1]++;
+        }
+        
+        for(int i= 0; i<n; i++){
+            if(student[i]==-1){
+                if(i-1>=0 && student[i-1] ==1) {
+                    student[i]++;
+                    student[i-1]--;
                 }
+                else if(i+1<n && student[i+1] ==1) {
+                    student[i+1]--;
+                    student[i]++;   
+                }
+                else answer--;
             }
         }
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
-                    reserve[j] = -1;
-                    student++;
-                    break;
-                }
-            }
-        }
-        return student;
+        
+        return answer;
     }
 }
