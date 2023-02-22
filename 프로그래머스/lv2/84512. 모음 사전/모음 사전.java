a103d84c29c2e7c65196279e5f116c4f461d2347
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-22 18:22:20 +0900

    [level 2] Title: 모음 사전, Time: 14.73 ms, Memory: 68.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    static char[] w = {'A','E','I','O','U'};
+    static List<String> results = new ArrayList<>();
+    public int solution(String word) {
+        int answer = 0;
+        findResult(0,"");
+        for(int i = 1; i < results.size(); i++){
+            if(results.get(i).equals(word)){
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+    
+    public void findResult(int index,String result){
+        results.add(result);
+        if(index == 5)return;
+        for(int i = 0; i < 5; i++){
+            findResult(index+1,result + w[i]);
+        }
+    }
+}
