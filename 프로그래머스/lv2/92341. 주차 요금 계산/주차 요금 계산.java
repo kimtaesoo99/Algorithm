24412a33c9efbf140e4b0845d1236083f9c54b54
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-24 00:51:04 +0900

    [level 2] Title: 주차 요금 계산, Time: 5.44 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,72 +1,55 @@
 import java.util.*;
+
 class Solution {
-   
-    static Map<String, String> map;
-    static Map<String, Integer> feesMap;
-    static Set<String> CarNUmber = new HashSet<>();
+    static Map<String,String> carAndInTime;
+    static Map<String,Integer> carAndFee;
+    public int[] solution(int[] fees, String[] records) {
+        carAndInTime = new HashMap<>();
+        carAndFee = new HashMap<>();
 
-    static int[] cost;
-
-    public static int[] solution(int[] fees, String[] records) {
-        map = new HashMap<>();
-        feesMap = new HashMap<>();
-
-        for (int i = 0; i < records.length; i++) {
-            if (records[i].endsWith("IN")) {
-                map.put(records[i].substring(6, 10), records[i].substring(0, 5));
-                CarNUmber.add(records[i].substring(6, 10));
-            } else if (records[i].endsWith("OUT")) {
-                getTime(records[i].substring(6, 10), records[i].substring(0, 5));
-                map.remove(records[i].substring(6, 10));
+        for(String s : records){
+            String[] info = s.split(" ");
+            String time = info[0];
+            String carNumber = info[1];
+            String type = info[2];
+            if(type.equals("IN")){
+                carAndInTime.put(carNumber,time);
+            }else {
+                carAndFee.put(carNumber, carAndFee.getOrDefault(carNumber,0) + getTime(time,carNumber));
+                carAndInTime.remove(carNumber);
             }
         }
-
-
-        for (String key : map.keySet()) {
-            // 이렇게 하면, map에 남아있는 키 값만 뽑아서 쓸 수 있음
-            getTimeNotOut(key);
+        Set<String> set = carAndInTime.keySet();
+        for(String carNumber : set){
+            carAndFee.put(carNumber, carAndFee.getOrDefault(carNumber,0) + getTime("23:59",carNumber));
         }
-
-        cost = new int[feesMap.size()];
-        getCost(fees);
-
-        return cost;
+        return carAndFee.entrySet().stream().sorted(Map.Entry.comparingByKey())
+                .map(i -> getFee((carAndFee.get(i.getKey())),fees)).mapToInt(Integer::intValue)
+                .toArray();
     }
 
-    //시간을 저장하는 메서드
-    public static void getTime(String CarNumber, String time) {
-        int aHour = Integer.parseInt(time.substring(0, 2));
-        int bHour = Integer.parseInt(map.get(CarNumber).substring(0, 2));
-        int aMin = Integer.parseInt(time.substring(3, 5));  // 퇴장시간의 시간중 분단위
-        int bMIn = Integer.parseInt(map.get(CarNumber).substring(3, 5));  //출입시간의 시간중 분단위
+    public int getTime(String time, String carNumber){
+        String[] inTime = carAndInTime.get(carNumber).split(":");
+        int inTimeHour = Integer.parseInt(inTime[0]);
+        int inMin = Integer.parseInt(inTime[1]);
 
-        feesMap.put(CarNumber, feesMap.getOrDefault(CarNumber, 0) + (aHour * 60 + aMin) - (bHour * 60 + bMIn));
+        String[] outTime = time.split(":");
+        int outTimeHour = Integer.parseInt(outTime[0]);
+        int outMin = Integer.parseInt(outTime[1]);
+
+        int totalTime = (outTimeHour - inTimeHour)*60 +outMin - inMin;
+        return totalTime;
     }
 
-    public static void getTimeNotOut(String CarNumber) {
-        int bHour = Integer.parseInt(map.get(CarNumber).substring(0, 2));
-        int bMIn = Integer.parseInt(map.get(CarNumber).substring(3, 5));
-
-
-        feesMap.put(CarNumber, feesMap.getOrDefault(CarNumber, 0) + (23 * 60 + 59) - (bHour * 60 + bMIn));
-    }
-
-    public static void getCost(int[] fees) {
-        List<String> temp = new ArrayList<>();
-        for (String num : CarNUmber) {
-            temp.add((num));
-        }
-        Collections.sort(temp);
-
-        int index = 0;
-        for (int i = 0; i < temp.size(); i++) {
-            int time = feesMap.get((temp.get(i)));
-            if (time <= fees[0]) cost[index] = fees[1];
-            else {
-                cost[index] = (int) ((Math.ceil((time - fees[0]) / (double) fees[2]) * 1.0) * fees[3]) + fees[1];
-            }
-            index++;
+    public int getFee(int time, int[] fees){
+        int basicTime = fees[0];
+        int basicFee = fees[1];
+        int overTime = fees[2];
+        int overFee = fees[3];
+        if(time<=basicTime)return basicFee;
+        else{
+            int over = time - basicTime;
+            return (int)Math.ceil(over/(double)overTime)*overFee + basicFee;
         }
     }
 }
-
