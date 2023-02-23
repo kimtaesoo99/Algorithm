619b5e17d95e1dfda89b6c55056c85be78c31a43
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-23 23:10:13 +0900

    [level 2] Title: 전화번호 목록, Time: 275.35 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,15 +1,16 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         Arrays.sort(phone_book);
-        for (int i = 0; i < phone_book.length-1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
+        for(int i = 0; i < phone_book.length-1; i++){
+            if(phone_book[i+1].startsWith(phone_book[i])){
                 answer = false;
                 break;
             }
         }
+
         return answer;
     }
 }
