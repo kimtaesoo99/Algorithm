223888799aa5777bb99ffe8d44366114d9783a63
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-24 19:46:07 +0900

    [level 3] Title: 가장 먼 노드, Time: 33.08 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,48 +1,46 @@
 import java.util.*;
 class Solution {
     static List<Integer>[] nodes;
-    static int[] result;
+    static int[] distance;
     public int solution(int n, int[][] edge) {
-        int answer = 1;
         nodes = new ArrayList[n+1];
-        for(int i = 1; i <= n; i++){
-            nodes[i] = new ArrayList<>();
-        }
-        result = new int[n+1];
-        for(int i = 0; i < edge.length; i++){
+        distance = new int[n+1];
+        for(int i =1 ; i <=n; i++)nodes[i] = new ArrayList<>();
+        
+        for(int i = 0 ; i < edge.length; i++){
             int from = edge[i][0];
             int to = edge[i][1];
             nodes[from].add(to);
             nodes[to].add(from);
         }
+        
         bfs(1);
-
-        int max = 0;
-        for(int i = 1; i <=n; i++){
-            if(result[i]>max){
-                answer = 1;
-                max = result[i];
-            }else if(result[i]==max)answer++;
+        int count = 0;
+        int maxDistance = 0;
+        for(int i =1; i<=n;i++){
+            if(maxDistance<distance[i]){
+                maxDistance = distance[i];
+                count = 1;
+            }
+            else if(maxDistance==distance[i])count++;
         }
-        return answer;
+        
+        return count;
     }
     
-    public void bfs(int now){
+    public void bfs(int start){
         Queue<Integer> q = new LinkedList<>();
-        q.offer(now);
-        result[now] = 1;
+        q.offer(start);
+        distance[start] = 1;
         
         while(!q.isEmpty()){
-            int nowNode = q.poll();
-            
-            for(int i=0; i<nodes[nowNode].size(); i++){
-                int nextNode = nodes[nowNode].get(i);
-                if(result[nextNode] ==0){
-                    result[nextNode] = result[nowNode] + 1;
-                    q.offer(nextNode);
+            int now = q.poll();
+            for(int next : nodes[now]){
+                if(distance[next]==0){
+                    distance[next] = distance[now] + 1;
+                    q.offer(next);
                 }
             }
         }
-        
     }
 }
