83c6323a374cc4c260015107a057385c5a0e3b01
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-24 17:42:59 +0900

    [level 2] Title: 타겟 넘버, Time: 1.01 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,21 +1,18 @@
 import java.util.*;
-
 class Solution {
-    static int answer = 0;
+    static int count = 0;
     public int solution(int[] numbers, int target) {
-
-        dfs(numbers, target,0,0);
-        return answer;
+        findTargetNumber(0,0,numbers,target);
+        return count;
     }
+
     
-    public void dfs(int[] numbers, int target, int sum, int index){
+    public void findTargetNumber(int index, int sum,int[] numbers, int target){
         if(index == numbers.length){
-            if(sum==target)answer++;
+            if(target==sum)count++;
             return;
         }
-        
-        dfs(numbers, target, sum+numbers[index], index+1);
-        dfs(numbers, target, sum-numbers[index], index+1);
-    
+        findTargetNumber(index+1,sum+numbers[index],numbers,target);
+        findTargetNumber(index+1,sum-numbers[index],numbers,target);
     }
 }
