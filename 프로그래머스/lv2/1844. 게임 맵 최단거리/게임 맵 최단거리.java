0824fa45195a3ad8ee4810a3d5e229df084bd432
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-24 18:55:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.93 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,34 +1,35 @@
 import java.util.*;
 class Solution {
     static int[][] result;
+    static int n;
+    static int m;
+    static int[] dy = {1,0,-1,0};
+    static int[] dx = {0,-1,0,1};
     public int solution(int[][] maps) {
-        result = new int[maps.length][maps[0].length];
-        bfs(0,0, maps);
-        return result[maps.length-1][maps[0].length-1]!=0?result[maps.length-1][maps[0].length-1]:-1;
+        n = maps.length;
+        m = maps[0].length;
+        result = new int[n][m];
+        findDistance(0,0,maps);
+        return result[n-1][m-1]==0?-1:result[n-1][m-1];
     }
     
-    public void bfs(int y, int x, int[][] maps){
+    public void findDistance(int y,int x,int[][] maps){
         Queue<int[]> q = new LinkedList<>();
-        result[y][x] = 1;
         q.offer(new int[]{y,x});
-        
-        int[] dy = {1,0,-1,0};
-        int[] dx = {0,1,0,-1};
-        
+        result[y][x] =1;
         while(!q.isEmpty()){
-            int[] poll = q.poll();
-            for(int i = 0; i < 4; i++){
-                int moveY = poll[0] + dy[i];
-                int moveX = poll[1] + dx[i];
+            int[] move = q.poll();
+            for(int i = 0; i<4; i++){
+                int moveY = move[0] + dy[i];
+                int moveX = move[1] + dx[i];
                 
-                if(0<=moveY && moveY < maps.length && 0<= moveX && moveX <maps[0].length){
-                    if(maps[moveY][moveX]==1 && result[moveY][moveX] ==0){
-                        result[moveY][moveX] = result[poll[0]][poll[1]] +1;
-                        q.offer(new int[]{moveY,moveX});
-                    }
+                if(0<=moveY && moveY< n && 0<= moveX && moveX <m){
+                    if(maps[moveY][moveX]==1 && result[moveY][moveX]==0){
+                    result[moveY][moveX] = result[move[0]][move[1]] +1;
+                    q.offer(new int[]{moveY,moveX});
+                }
                 }
             }
         }
-        
     }
 }
