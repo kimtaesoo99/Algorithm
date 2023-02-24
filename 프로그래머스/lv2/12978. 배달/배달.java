4bc11345c06e9969e8c166c23740ad8b8a4ccb09
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-24 21:04:13 +0900

    [level 2] Title: 배달, Time: 1.18 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.java
+++ 프로그래머스/lv2/12978. 배달/배달.java
@@ -4,27 +4,28 @@ class Solution {
     static List<Node>[] nodes;
     static int[] distance;
     public int solution(int N, int[][] road, int K) {
-        int answer = 0;
         visited = new boolean[N+1];
         nodes = new ArrayList[N+1];
         distance = new int[N+1];
-        
-        for(int i =1 ; i<=N; i++){
-            nodes[i] = new ArrayList<>();
+        for(int i = 1; i <= N; i++){
             distance[i] = Integer.MAX_VALUE;
+            nodes[i] = new ArrayList<>();
         }
-        for(int i = 0; i <road.length; i++){
+        for(int i = 0; i<road.length; i++){
             int from = road[i][0];
             int to = road[i][1];
             int cost = road[i][2];
             nodes[from].add(new Node(to,cost));
             nodes[to].add(new Node(from,cost));
         }
+        
         findDistance(1);
-        for(int i =1; i<=N; i++){
+        int answer = 0;
+        for(int i = 1; i <=N; i++){
             if(distance[i]<=K)answer++;
         }
-        
+
+
         return answer;
     }
     
@@ -39,7 +40,7 @@ class Solution {
             visited[now] = true;
             for(Node node : nodes[now]){
                 if(!visited[node.link]){
-                    if(distance[now] + node.cost < distance[node.link]){
+                    if(distance[now]+node.cost < distance[node.link]){
                         distance[node.link] = distance[now] + node.cost;
                         q.offer(new Node(node.link,distance[node.link]));
                     }
@@ -49,17 +50,16 @@ class Solution {
     }
 }
 
-
 class Node implements Comparable<Node>{
     int link;
     int cost;
     
     @Override
     public int compareTo(Node n){
-        return this.cost - n.cost;
+        return this.cost-n.cost;
     }
     
-    public Node(int link, int cost){
+    public Node(int link , int cost){
         this.link = link;
         this.cost = cost;
     }
