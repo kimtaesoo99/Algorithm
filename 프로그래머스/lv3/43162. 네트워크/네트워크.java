4759485fdf9ce818498daf1cf8f7401735449680
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-24 17:52:18 +0900

    [level 3] Title: 네트워크, Time: 0.23 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -1,23 +1,23 @@
 class Solution {
+    static int count = 0;
     static boolean[] visited;
     public int solution(int n, int[][] computers) {
-        int answer = 0;
         visited = new boolean[n];
-        for(int i = 0; i < n; i++){
-            if(!visited[i]){
-                dfs(i,computers);
-                answer++;
+        for(int i = 0; i < computers.length; i++){
+            for(int j = 0; j < computers.length; j++){
+                if(!visited[i] && computers[i][j]==1){
+                    findNetwork(j,computers);
+                    count++;
+                }
             }
         }
-        return answer;
+        return count;
     }
-    
-    public void dfs(int i, int[][] computers){
-        visited[i] = true;
-        
-        for(int j  = 0; j < computers.length; j++){
-            if(!visited[j] && i!=j && computers[i][j] ==1){
-                dfs(j,computers);
+    public void findNetwork(int index, int[][] computers){
+        visited[index] = true;
+        for(int i = 0; i < computers.length; i++){
+            if(!visited[i]&& computers[index][i]==1){
+                findNetwork(i , computers);
             }
         }
     }
