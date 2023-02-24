131d8b0074f97a109f6ecdf72475e74cdeb9e0db
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-24 22:34:38 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.08 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,19 +1,18 @@
 class Solution {
     public String solution(String number, int k) {
+        int start = 0;
         StringBuilder sb = new StringBuilder();
-        int index = 0;
-        for (int i = 0; i < number.length() - k; i++){
+        for(int i = 0; i < number.length()-k; i++){
             int max = 0;
-
-            for (int j = index; j <= i + k; j++){
-                int num = number.charAt(j) -'0';
-                if (max < num){
-                    max = num;
-                    index = j + 1;
+            for(int j = start; j<=i+k; j++){
+                if(max < number.charAt(j)-'0'){
+                    max = number.charAt(j)-'0';
+                    start = j+1;
                 }
             }
             sb.append(max);
         }
+        
         return sb.toString();
     }
 }
