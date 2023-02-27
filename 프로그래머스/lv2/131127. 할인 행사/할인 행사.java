aff55389d50dfbc8f39a3b0bbd7f4a93afb2d9b4
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-27 14:05:14 +0900

    [level 2] Title: 할인 행사, Time: 1.07 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        Map<String,Integer> map = new HashMap<>();
+        for(int i = 0; i < want.length; i++){
+            map.put(want[i],number[i]);
+        }
+
+        int day = 0;
+        for(int i = 0; i < discount.length; i++){
+            if(wantBuy(want, discount[i])){
+                map.put(discount[i],map.get(discount[i])-1);
+            }
+            if(i>=10){
+                if(wantBuy(want, discount[i-10])){
+                    map.put(discount[i-10],map.get(discount[i-10])+1);
+                }
+            }
+            if(canBuy(map)){
+                day++;
+            }
+        }
+        return day;
+    }
+
+    public boolean canBuy(Map<String,Integer> map){
+        return map.values().stream().allMatch(i-> i == 0);
+    }
+
+    public boolean wantBuy(String[] want , String a){
+        for(String s: want){
+            if(s.equals(a))return true;
+        }
+        return false;
+    }
+}
