8953289de0150c6bd0574365fee6c7104049f3d7
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-03-02 22:17:27 +0900

    [level 2] Title: 배달, Time: 0.75 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.java
+++ 프로그래머스/lv2/12978. 배달/배달.java
@@ -1,35 +1,36 @@
 import java.util.*;
 class Solution {
-    static boolean[] visited;
     static List<Node>[] nodes;
+    static boolean[] visited;
     static int[] distance;
     public int solution(int N, int[][] road, int K) {
-        visited = new boolean[N+1];
         nodes = new ArrayList[N+1];
+        visited = new boolean[N+1];
         distance = new int[N+1];
-        for(int i = 1; i <= N; i++){
-            distance[i] = Integer.MAX_VALUE;
+        
+        for(int i = 0; i <=N; i++){
             nodes[i] = new ArrayList<>();
+            distance[i] = Integer.MAX_VALUE;
         }
-        for(int i = 0; i<road.length; i++){
-            int from = road[i][0];
-            int to = road[i][1];
-            int cost = road[i][2];
+        
+        for(int[] i : road){
+            int from  = i[0];
+            int to = i[1];
+            int cost = i[2];
             nodes[from].add(new Node(to,cost));
             nodes[to].add(new Node(from,cost));
         }
         
-        findDistance(1);
+        dij(1);   
         int answer = 0;
-        for(int i = 1; i <=N; i++){
+        for(int i =1; i <=N; i++){
             if(distance[i]<=K)answer++;
         }
-
-
+        
         return answer;
     }
     
-    public void findDistance(int start){
+    public void dij(int start){
         PriorityQueue<Node> q = new PriorityQueue<>();
         q.offer(new Node(start,0));
         distance[start] = 0;
@@ -38,11 +39,12 @@ class Solution {
             Node nowNode = q.poll();
             int now = nowNode.link;
             visited[now] = true;
+            
             for(Node node : nodes[now]){
                 if(!visited[node.link]){
-                    if(distance[now]+node.cost < distance[node.link]){
+                    if(distance[now] + node.cost < distance[node.link]){
                         distance[node.link] = distance[now] + node.cost;
-                        q.offer(new Node(node.link,distance[node.link]));
+                        q.offer(new Node(node.link, distance[node.link]));
                     }
                 }
             }
@@ -50,16 +52,18 @@ class Solution {
     }
 }
 
+
 class Node implements Comparable<Node>{
     int link;
     int cost;
     
     @Override
     public int compareTo(Node n){
-        return this.cost-n.cost;
+        return this.cost - n.cost;
     }
     
-    public Node(int link , int cost){
+    
+    public Node(int link, int cost){
         this.link = link;
         this.cost = cost;
     }
