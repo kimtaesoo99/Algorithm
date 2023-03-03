893dfade6c21d4ffc5209b26aa3e3693ed8a2153
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-03-03 18:53:12 +0900

    [level 3] Title: 가장 먼 노드, Time: 34.73 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,28 +2,32 @@ import java.util.*;
 class Solution {
     static List<Integer>[] nodes;
     static int[] distance;
+    static boolean[] visited;
     public int solution(int n, int[][] edge) {
         nodes = new ArrayList[n+1];
         distance = new int[n+1];
-        for(int i =1 ; i <=n; i++)nodes[i] = new ArrayList<>();
+        visited = new boolean[n+1];
+        for(int i = 0; i <= n; i++)nodes[i] = new ArrayList<>();
         
-        for(int i = 0 ; i < edge.length; i++){
-            int from = edge[i][0];
-            int to = edge[i][1];
+        for(int[] e : edge){
+            int from = e[0];
+            int to = e[1];
             nodes[from].add(to);
             nodes[to].add(from);
         }
         
         bfs(1);
         int count = 0;
-        int maxDistance = 0;
-        for(int i =1; i<=n;i++){
-            if(maxDistance<distance[i]){
-                maxDistance = distance[i];
+        int max = 0;
+        for(int i =1 ; i<=n; i++){
+            if(distance[i] > max){
+                max = distance[i];
                 count = 1;
+            }else if(distance[i] == max){
+                count++;
             }
-            else if(maxDistance==distance[i])count++;
         }
+        
         
         return count;
     }
@@ -32,13 +36,13 @@ class Solution {
         Queue<Integer> q = new LinkedList<>();
         q.offer(start);
         distance[start] = 1;
-        
         while(!q.isEmpty()){
-            int now = q.poll();
-            for(int next : nodes[now]){
-                if(distance[next]==0){
-                    distance[next] = distance[now] + 1;
-                    q.offer(next);
+            int next = q.poll();
+            for(int node : nodes[next]){
+                if(!visited[node] && distance[node]==0){
+                    distance[node] = distance[next] + 1;
+                    visited[node] = true;
+                    q.offer(node);
                 }
             }
         }
