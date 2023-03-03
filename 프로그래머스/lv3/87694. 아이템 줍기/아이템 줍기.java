f3df6774092fa21f778293a3914f471c5f583498
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-03-03 14:37:25 +0900

    [level 3] Title: 아이템 줍기, Time: 0.43 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,53 +1,52 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
+import java.util.*;
 class Solution {
     static int[][] map;
-    static int[] dx = {1,0,-1,0};
-    static int[] dy = {0,1,0,-1};
-    static int answer;
-    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+    static int[][] distance;
+    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY){
         map = new int[101][101];
-        for (int[] r : rectangle)fill(2*r[0],2*r[1],2*r[2],2*r[3]);
-        bfs(2*characterX,2*characterY,2*itemX,2*itemY);
-        return answer;
+        distance = new int[101][101];
+        initMap(rectangle);
+        bfs(2*characterX, 2*characterY);
+        return distance[2*itemY][2*itemX]/2;
     }
-
-    public void fill(int x1, int y1, int x2, int y2){
-        for (int i = x1; i<=x2; i++){
-            for (int j = y1; j<=y2; j++){
-                if (map[i][j] == 2)continue;
-                map[i][j] = 2;
-                if (i==x1||i==x2||j==y1||j==y2)map[i][j] = 1;
-            }
-        }
-    }
-
-    public void bfs(int startX, int startY, int itemX, int itemY){
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{startX,startY});
-        int[][] cost = new int[101][101];
-        cost[startX][startY] = 1;
-
-        while (!q.isEmpty()){
-            int[] move = q.poll();
-
-            for (int i = 0; i < 4; i++){
-                int moveX = move[0] + dx[i];
-                int moveY = move[1] + dy[i];
-
-                if (validation(moveX,moveY))continue;
-
-                if (map[moveX][moveY] == 1 && cost[moveX][moveY] == 0){
-                    cost[moveX][moveY] = cost[move[0]][move[1]]+1;
-                    q.offer(new int[]{moveX, moveY});
+    
+    public void initMap(int[][] rectangle){
+        for(int[] r : rectangle){
+            int x1 = 2*r[0];
+            int y1 = 2*r[1];
+            int x2 = 2*r[2];
+            int y2 = 2*r[3];
+            
+            for(int i = y1; i<=y2; i++){
+                for(int j = x1; j<=x2; j++){
+                    if(map[i][j]==2)continue;
+                    map[i][j] = 2;
+                    if(i==y1 || i == y2|| j==x1||j==x2)map[i][j] = 1;
                 }
             }
         }
-        answer = cost[itemX][itemY]/2;
     }
-
-    public boolean validation(int x, int y){
-        return (0>x || 0>y || x>100 ||y>100);
+    
+    public void bfs(int x, int y){
+        Queue<int[]> q = new LinkedList<>();
+        distance[y][x] = 1;
+        q.offer(new int[]{y,x});
+        int[] dy = {1,0,-1,0};
+        int[] dx = {0,1,0,-1};
+        while(!q.isEmpty()){
+            int[] move = q.poll();
+            for(int i = 0 ; i <4; i++){
+                int moveY = move[0] + dy[i];
+                int moveX = move[1] + dx[i];
+                
+                if(0<= moveX && moveX <=100 && 0<=moveY && moveY <=100){
+                    if(map[moveY][moveX] == 1 && distance[moveY][moveX] ==0){
+                        distance[moveY][moveX] = distance[move[0]][move[1]]+1;
+                        q.offer(new int[]{moveY,moveX});
+                    }
+                }
+            }
+        }
+        
     }
 }
