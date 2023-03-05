e93a5ff7473d13204ab6ecebcedad1447208784b
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-03-05 19:04:36 +0900

    [Gold III] Title: 순회강연, Time: 684 ms, Memory: 21072 KB -BaekjoonHub

---
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<Work> workList = new ArrayList<>();
+        int maxDay = 0;
+        for (int i = 0; i < n; i++){
+            String[] info = br.readLine().split(" ");
+            workList.add(new Work(Integer.parseInt(info[0]), Integer.parseInt(info[1])));
+            maxDay = Math.max(maxDay, Integer.parseInt(info[0]));
+        }
+        int sum = 0;
+        for (int i = maxDay; i>=1; i--){
+            int maxIndex = -1;
+            int maxValue = 0;
+            for (int j = 0; j < workList.size(); j++){
+                if (workList.get(j).day >=i){
+                    if (workList.get(j).score > maxValue){
+                        maxValue = workList.get(j).score;
+                        maxIndex = j;
+                    }
+                }
+            }
+            sum += maxValue;
+            if (maxIndex!=-1) workList.remove(maxIndex);
+        }
+        System.out.println(sum);
+    }
+}
+
+class Work{
+    int score;
+    int day;
+
+    public Work(int score, int day) {
+        this.score = score;
+        this.day = day;
+    }
+}
