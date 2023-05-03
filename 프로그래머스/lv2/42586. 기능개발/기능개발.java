b58efb693dc366cb49ba0a3a281d645ff5ca7f49
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-05-03 22:01:47 +0900

    [level 2] Title: 기능개발, Time: 2.16 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -1,37 +1,15 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> q = new LinkedList<>();
+        int[] answer = new int[100];
+        int day = -1;
         for (int i = 0; i < progresses.length; i++) {
-            int left = (100 - progresses[i]);
-            int day = left / speeds[i];
-            if (left % speeds[i] != 0) {
+            while ((progresses[i] + speeds[i] * day) < 100) {
                 day++;
             }
-            q.offer(day);
+            answer[day]++;
         }
-
-        List<Integer> result = new ArrayList<>();
-        int index = -1;
-        int max = -1;
-        while (!q.isEmpty()) {
-            if (q.peek() > max) {
-                int poll = q.poll();
-                result.add(1);
-                max = poll;
-                index++;
-                continue;
-            }
-            result.set(index, result.get(index) + 1);
-            q.poll();
-        }
-        return result.stream()
-                .mapToInt(Integer::intValue)
-                .toArray();
+        return Arrays.stream(answer).filter(i -> i != 0).toArray();
     }
 }
