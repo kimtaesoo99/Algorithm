735c682a399f1157d11a630ad8ff803a21f8b0be
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-05-05 12:29:55 +0900

    [level 2] Title: 튜플, Time: 0.72 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -1,27 +1,19 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        String a = s.replace("{", "");
-        String[] arr = a.substring(0, a.length() - 2).split("},");
-        Arrays.sort(arr, (s1,s2) -> s1.length() - s2.length());
-        List<Integer> result = new ArrayList<>();
-        for (String str : arr) {
-            List<Integer> now = Arrays.stream(str.split(","))
-                    .map(Integer::parseInt)
-                    .collect(Collectors.toList());
-            addValue(result, now);
+        Set<String> set = new HashSet<>();
+        String[] arr = s.replaceAll("[{]", " ")
+                .replaceAll("[}]", " ")
+                .trim().split(" , ");
+        Arrays.sort(arr, (a, b)->{return a.length() - b.length();});
+        int[] answer = new int[arr.length];
+        int idx = 0;
+        for(String s1 : arr) {
+            for(String s2 : s1.split(",")) {
+                if(set.add(s2)) answer[idx++] = Integer.parseInt(s2);
+            }
         }
-        return result.stream().mapToInt(Integer::intValue).toArray();
-    }
-
-    private void addValue(List<Integer> result, List<Integer> now) {
-        for (Integer r : result) {
-            now.remove(r);
-        }
-        result.add(now.get(0));
+        return answer;
     }
 }
