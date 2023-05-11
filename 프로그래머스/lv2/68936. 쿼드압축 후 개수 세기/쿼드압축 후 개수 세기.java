5cc2d2fd77cbbaa921572af402339ed9bfc08ef7
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-05-11 13:43:32 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 5.34 ms, Memory: 94.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -0,0 +1,37 @@
+class Solution {
+    static int zero = 0;
+    static int one = 0;
+
+    public int[] solution(int[][] arr) {
+        compression(arr.length, arr, 0, 0);
+        return new int[]{zero, one};
+    }
+
+    private void compression(int length, int[][] arr, int y, int x) {
+        if (isSameArea(length, arr, y, x)) {
+            if (arr[y][x] == 1) {
+                one += 1;
+                return;
+            }
+            zero += 1;
+            return;
+        }
+        int halfLength = length / 2;
+        compression(halfLength, arr, y, x);
+        compression(halfLength, arr, y + halfLength, x);
+        compression(halfLength, arr, y, x + halfLength);
+        compression(halfLength, arr, y + halfLength, x + halfLength);
+    }
+
+    private boolean isSameArea(int length, int[][] arr, int y, int x) {
+        int first = arr[y][x];
+        for (int i = y; i < y + length; i++) {
+            for (int j = x; j < x + length; j++) {
+                if (arr[i][j] != first) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
