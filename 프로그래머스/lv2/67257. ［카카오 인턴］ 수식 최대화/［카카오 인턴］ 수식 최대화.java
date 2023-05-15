81e101ed83c95e0d3c793ddafd1d757e1d7a22ed
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-05-15 13:31:47 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 15.16 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -4,11 +4,30 @@ import java.util.List;
 class Solution {
     static long answer = 0;
     static String expression;
+    static List<Long> numbers;
+    static List<Character> operation;
 
     public long solution(String expression) {
         this.expression = expression;
+        divisionExpression();
         permutation(0, 3, new char[]{'+', '-', '*'}, new boolean[3], "");
         return answer;
+    }
+
+    private void divisionExpression() {
+        StringBuilder number = new StringBuilder();
+        numbers = new ArrayList<>();
+        operation = new ArrayList<>();
+        for (int i = 0; i < expression.length(); i++) {
+            if (expression.charAt(i) == '+' || expression.charAt(i) == '-' || expression.charAt(i) == '*') {
+                numbers.add(Long.parseLong(number.toString()));
+                number = new StringBuilder();
+                operation.add(expression.charAt(i));
+                continue;
+            }
+            number.append(expression.charAt(i));
+        }
+        numbers.add(Long.parseLong(number.toString()));
     }
 
     private void permutation(int index, int target, char[] operation, boolean[] check, String result) {
@@ -26,44 +45,32 @@ class Solution {
     }
 
     private void calc(String operator) {
-        StringBuilder number = new StringBuilder();
-        List<Long> numbers = new ArrayList<>();
-        List<Character> operation = new ArrayList<>();
-        for (int i = 0; i < expression.length(); i++) {
-            if (expression.charAt(i) == '+' || expression.charAt(i) == '-' || expression.charAt(i) == '*') {
-                numbers.add(Long.parseLong(number.toString()));
-                number = new StringBuilder();
-                operation.add(expression.charAt(i));
-                continue;
-            }
-            number.append(expression.charAt(i));
-        }
-        numbers.add(Long.parseLong(number.toString()));
-
+        List<Long> nowNumbers = new ArrayList<>(numbers);
+        List<Character> nowOperation = new ArrayList<>(operation);
         for (int i = 0; i < operator.length(); i++) {
             char now = operator.charAt(i);
             int index;
-            while (operation.size() != 0) {
-                index = operation.indexOf(now);
+            while (nowOperation.size() != 0) {
+                index = nowOperation.indexOf(now);
                 if (index == -1) {
                     break;
                 }
                 switch (now) {
                     case '+':
-                        numbers.add(index, numbers.get(index) + numbers.get(index + 1));
+                        nowNumbers.add(index, nowNumbers.get(index) + nowNumbers.get(index + 1));
                         break;
                     case '-':
-                        numbers.add(index, numbers.get(index) - numbers.get(index + 1));
+                        nowNumbers.add(index, nowNumbers.get(index) - nowNumbers.get(index + 1));
                         break;
                     case '*':
-                        numbers.add(index, numbers.get(index) * numbers.get(index + 1));
+                        nowNumbers.add(index, nowNumbers.get(index) * nowNumbers.get(index + 1));
                         break;
                 }
-                numbers.remove(index + 1);
-                numbers.remove(index + 1);
-                operation.remove(index);
+                nowNumbers.remove(index + 1);
+                nowNumbers.remove(index + 1);
+                nowOperation.remove(index);
             }
         }
-        answer = Math.max(answer, Math.abs(numbers.get(0)));
+        answer = Math.max(answer, Math.abs(nowNumbers.get(0)));
     }
 }
