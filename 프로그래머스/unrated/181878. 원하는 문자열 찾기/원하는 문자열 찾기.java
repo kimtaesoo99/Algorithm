60e0eedf1fe9f1c041f6790279993355c05f6fd1
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-05-21 15:12:58 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 5.82 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String myString, String pat) {
+        return myString.toLowerCase().contains(pat.toLowerCase()) ? 1 : 0;
+    }
+}
