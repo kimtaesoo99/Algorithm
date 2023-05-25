6667206488cb2364980344c9ce9749fadcf6ca55
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-05-25 13:51:29 +0900

    [unrated] Title: n개 간격의 원소들, Time: 2.74 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -0,0 +1,7 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        return IntStream.rangeClosed(0, num_list.length - 1).filter(i -> i % n == 0).map(i -> num_list[i]).toArray();
+    }
+}
