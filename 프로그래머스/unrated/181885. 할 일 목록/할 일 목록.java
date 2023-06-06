6b1ccf2639ba15f2cefad5a47d7460fbd90cf244
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-06-06 13:26:48 +0900

    [unrated] Title: 할 일 목록, Time: 0.47 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < todo_list.length; i++) {
+            if (!finished[i]){
+                list.add(todo_list[i]);
+            }
+        }
+        return list.toArray(String[]::new);
+    }
+}
