88db5c8dbed73eed04a34154ab261e799f58ab51
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2023-07-10 07:09:06 +0900

    [level 2] Title: n^2 배열 자르기, Time: 13.44 ms, Memory: 93.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        List<Long> list = new ArrayList<>();
+        for (long i = left; i <= right; i++) {
+            list.add(Math.max(i / n, i % n) + 1);
+        }
+        return list.stream().mapToInt(Long::intValue).toArray();
+    }
+}
