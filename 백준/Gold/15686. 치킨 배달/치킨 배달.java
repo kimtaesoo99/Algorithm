9df5d6ade67f4e368c4a28ce9fb0a591a3c23ca5
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-01-21 16:53:42 +0900

    [Gold V] Title: 치킨 배달, Time: 408 ms, Memory: 22660 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,71 +1,69 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    static int N, M;
-    static int[][] map;
-    static ArrayList<Point> person = new ArrayList<>();
-    static ArrayList<Point> chicken = new ArrayList<>();
-    static int result = Integer.MAX_VALUE;
-    static boolean[] open;
+
+    private static int result = Integer.MAX_VALUE;
+    private static boolean[] visited;
+    private static List<int[]> home;
+    private static List<int[]> chicken;
+    private static int M;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
+        int N = sc.nextInt();
         M = sc.nextInt();
-        map = new int[N][N];
+
+        home = new ArrayList<>();
+        chicken = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                map[i][j] = sc.nextInt();
-                if (map[i][j] == 1) {
-                    person.add(new Point(i, j));
+                int info = sc.nextInt();
+                if (info == 1) {
+                    home.add(new int[]{i, j});
                 }
-                if (map[i][j] == 2) {
-                    chicken.add(new Point(i, j));
+                if (info == 2) {
+                    chicken.add(new int[]{i, j});
                 }
             }
         }
 
-        open = new boolean[chicken.size()];
+        visited = new boolean[chicken.size()];
 
-        DFS(0, 0);
+        combination(0, -1, new int[M]);
+
         System.out.println(result);
     }
 
-    public static void DFS(int start, int cnt) {
-        if (cnt == M) {
-            int sum = 0;
-            for (Point point : person) {
-                int minDistance = Integer.MAX_VALUE;
-                for (int j = 0; j < chicken.size(); j++) {
-                    if (open[j]) {
-                        int distance = Math.abs(point.x - chicken.get(j).x)
-                                + Math.abs(point.y - chicken.get(j).y);
-
-                        minDistance = Math.min(minDistance, distance);
-                    }
-                }
-                sum += minDistance;
-            }
-            result = Math.min(result, sum);
+    private static void combination(int index, int pre, int[] choice) {
+        if (index == M) {
+            result = Math.min(result, findDistance(choice));
             return;
         }
 
-        for (int i = start; i < chicken.size(); i++) {
-            open[i] = true;
-            DFS(i + 1, cnt + 1);
-            open[i] = false;
+        for (int i = 0; i < chicken.size(); i++) {
+            if (!visited[i] && i > pre) {
+                visited[i] = true;
+                choice[index] = i;
+                combination(index + 1, i, choice);
+                visited[i] = false;
+            }
         }
     }
-}
 
-class Point {
-    int x;
-    int y;
+    private static int findDistance(int[] choice) {
+        int sum = 0;
+        for (int[] ints : home) {
+            int min = Integer.MAX_VALUE;
 
-    Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+            for (int c : choice) {
+                int[] target = chicken.get(c);
+                min = Math.min(min, Math.abs(ints[0] - target[0]) + Math.abs(ints[1] - target[1]));
+            }
+            sum += min;
+        }
+        return sum;
     }
 }
