37198b3f4a6a202810175de8f563c9e169a33b98
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-01-29 13:56:18 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 232 ms, Memory: 18228 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -1,31 +1,39 @@
 import java.util.Scanner;
 
-public class Main {
-
-	static String recurTap = "";
-	public static void recursive(int n) {
-
-		String tap = recurTap;
-		
-		if(n == 0) {
-			System.out.println(tap + "\"재귀함수가 뭔가요?\"");
-			System.out.println(tap +"\"재귀함수는 자기 자신을 호출하는 함수라네\"");
-			System.out.println(tap + "라고 답변하였지.");
-			return;
-		}
-		System.out.println(tap + "\"재귀함수가 뭔가요?\"");
-		System.out.println(tap + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
-		System.out.println(tap + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
-		System.out.println(tap + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
-		recurTap += "____";
-		recursive(n - 1);
-		System.out.println(tap + "라고 답변하였지.");
-	}
+class Main {
 	
-	public static void main(String[] args) {
+	private static StringBuilder result = new StringBuilder();
+	
+	public static void main(String args[]) throws Exception {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
-		recursive(n);
+		result.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+		
+		print(0,n);
+		System.out.println(result);
+	}
+
+	private static void print(int index, int n) {
+		if (index == n) {
+			result.append(repeat(index) + "\"재귀함수가 뭔가요?\"\n");
+			result.append(repeat(index) + "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
+			result.append(repeat(index) + "라고 답변하였지.\n");
+			return;
+		}
+
+		result.append(repeat(index) + "\"재귀함수가 뭔가요?\"\n");
+		result.append(repeat(index) + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+		result.append(repeat(index) + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+		result.append(repeat(index) + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
+		print(index + 1, n);
+		result.append(repeat(index) + "라고 답변하였지.\n");
+	}
+	
+	private static String repeat(int n) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			sb.append("____");
+		}
+		return sb.toString();
 	}
 }
