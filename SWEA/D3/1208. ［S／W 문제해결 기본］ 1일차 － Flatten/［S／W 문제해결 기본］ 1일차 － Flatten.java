757573c8d73f99a7aa3f2f8ebe5196ea2e15dc25
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-01-30 14:17:18 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 211 ms, Memory: 28,564 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.TreeMap;
+import java.util.PriorityQueue;
 
 public class Solution {
 
@@ -14,35 +13,29 @@ public class Solution {
 		for (int test_case = 1; test_case <= T; test_case++) {
 			int temp = Integer.parseInt(br.readLine());
 
-			TreeMap<Integer, Integer> map = new TreeMap<>();
-
-			Arrays.stream(br.readLine().split(" ")).forEach(i -> {
-				Integer value = Integer.parseInt(i);
-				map.put(value, map.getOrDefault(value, 0) + 1);
-			});
-
-			for (int i = 0; i < temp; i++) {
-				Integer min = map.firstKey();
-				Integer max = map.lastKey();
-
-				remove(min, map);
-				remove(max, map);
-
-				map.put(min + 1, map.getOrDefault(min + 1, 0) + 1);
-				map.put(max - 1, map.getOrDefault(max - 1, 0) + 1);
+			PriorityQueue<Integer> pq1 = new PriorityQueue<>();
+			PriorityQueue<Integer> pq2 = new PriorityQueue<>((o1,o2) -> o2-o1);
+			
+			for(String s : br.readLine().split(" ")) {
+				Integer value = Integer.parseInt(s);
+				pq1.add(value);
+				pq2.add(value);
 			}
-
-			sb.append("#").append(test_case).append(" ").append(map.lastKey() - map.firstKey()).append("\n");
+			
+			for(int i = 0; i < temp; i++) {
+				Integer min = pq1.poll();
+				Integer max = pq2.poll();
+				pq1.remove(max);
+				pq2.remove(min);
+				pq1.add(max - 1);
+				pq1.add(min + 1);
+				pq2.add(max - 1);
+				pq2.add(min + 1);
+			}
+			
+			sb.append("#").append(test_case).append(" ").append(pq2.poll() - pq1.poll()).append("\n");
 		}
 
 		System.out.println(sb);
-	}
-
-	private static void remove(int key, TreeMap<Integer, Integer> map) {
-		map.put(key, map.getOrDefault(key, 0) - 1);
-
-		if (map.get(key) == 0) {
-			map.remove(key);
-		}
 	}
 }
