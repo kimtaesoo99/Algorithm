c3ba5cdfc28e8e38ee787e033f41c7a86be4bd5c
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-02-01 16:27:52 +0900

    [Gold V] Title: 신기한 소수, Time: 76 ms, Memory: 11312 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,40 +1,44 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        findResult(0, n);
-        System.out.println(sb);
-    }
+	private static StringBuilder sb = new StringBuilder();
 
-    private static void findResult(int num, int n) {
-        if (n == 0) {
-            if (isPrime(num)) {
-                sb.append(num).append("\n");
-                return;
-            }
-        }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		findResult(0, n);
+		System.out.println(sb);
+	}
 
-        for (int i = 0; i <= 9; i++) {
-            int next = num * 10 + i;
-            if (isPrime(next)) {
-                findResult(next, n - 1);
-            }
-        }
-    }
+	private static void findResult(int num, int n) {
+		if (n == 0) {
+			if (isPrime(num)) {
+				sb.append(num).append("\n");
+				return;
+			}
+		}
 
-    private static boolean isPrime(int num) {
-        if (num < 2) {
-            return false;
-        }
+		for (int i = 0; i < 10; i++) {
+			int next = num * 10 + i;
+			if (isPrime(next)) {
+				findResult(next, n - 1);
+			}
+		}
+	}
 
-        for (int i = 2; i <= Math.sqrt(num); i++){
-            if (num%i==0)return false;
-        }
+	private static boolean isPrime(int num) {
+		if (num < 2) {
+			return false;
+		}
 
-        return true;
-    }
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
