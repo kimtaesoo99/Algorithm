d71a42f301a4927ae07bc9587a386b93d8e9076e
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-02-02 15:01:42 +0900

    [Silver IV] Title: 카드2, Time: 140 ms, Memory: 43108 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,26 +1,26 @@
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        Queue q = new LinkedList();
-        int N =sc.nextInt();
 
-        for (int i=1;i<=N; i++){
-            q.offer(i);
-        }
-
-        while(true){
-            if(N==1)break;
-            q.poll();
-            N--;
-            if(N==1)break;
-            q.offer(q.poll());
-
-        }
-        System.out.println(q.peek());
-    }
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		Queue<Integer> q = new LinkedList<>();
+		
+		for(int i = 1; i <= n; i++) {
+			q.add(i);
+		}
+		
+		while(q.size() != 1) {
+			q.poll();
+			if (!q.isEmpty()) {
+				q.add(q.poll());
+			}
+		}
+		
+		System.out.println(q.poll());
+	}
 }
