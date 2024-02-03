80824bb8ab2589b9ca2bae32aabdab814f730cb5
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-02-04 08:29:38 +0900

    [Gold IV] Title: 인구 이동, Time: 4952 ms, Memory: 297252 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,90 +1,94 @@
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Main {
-    static int N;
-    static int L;
-    static int R;
-    static int[][] map;
-    static boolean[][] visited;
-    static int[] dy = {1, 0, -1, 0};
-    static int[] dx = {0, -1, 0, 1};
+class Main {
 
-    public static void main(String[] args) {
+    private static final int[] dy = {1, 0, -1, 0};
+    private static final int[] dx = {0, 1, 0, -1};
+
+    private static boolean change;
+    private static int[][] map;
+    private static boolean[][] visited;
+    private static int l;
+    private static int r;
+
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt(); 
-        L = sc.nextInt(); 
-        R = sc.nextInt(); 
+        int n = sc.nextInt();
+        l = sc.nextInt();
+        r = sc.nextInt();
 
-        map = new int[N][N];
-        visited = new boolean[N][N];
+        map = new int[n][n];
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 map[i][j] = sc.nextInt();
             }
         }
+
         int result = 0;
 
         while (true) {
-            int switchingCount = 0;
-            visited = new boolean[N][N];
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
+            visited = new boolean[n][n];
+            change = false;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
                     if (!visited[i][j]) {
-                        switchingCount++;
-                        bfs(i, j);
+                        bfs(i, j, n);
                     }
                 }
             }
-            if (switchingCount == N * N) {
+            if (!change) {
                 break;
             }
             result++;
         }
+
         System.out.println(result);
     }
 
-    private static void bfs(int y, int x) {
+    private static void bfs(int y, int x, int n) {
         Queue<int[]> q = new LinkedList<>();
         q.offer(new int[]{y, x});
-        visited[y][x] = true;
+        boolean[][] open = new boolean[n][n];
         int sum = map[y][x];
-        List<int[]> change = new ArrayList<>();
-        change.add(new int[]{y,x});
+        int count = 1;
+        open[y][x] = true;
+        visited[y][x] = true;
 
         while (!q.isEmpty()) {
-            int[] move = q.poll();
-            for (int i = 0; i < 4; i++) {
-                int moveY = move[0] + dy[i];
-                int moveX = move[1] + dx[i];
+            int[] now = q.poll();
 
-                if (validation(moveY, moveX)) {
-                    continue;
-                }
-                int gap = Math.abs(map[moveY][moveX] - map[move[0]][move[1]]);
-                if (!visited[moveY][moveX] && L <= gap && gap <= R) {
-                    visited[moveY][moveX] = true;
-                    change.add(new int[]{moveY, moveX});
-                    q.offer(new int[]{moveY, moveX});
-                    sum += map[moveY][moveX];
+            for (int i = 0; i < 4; i++) {
+                int moveY = now[0] + dy[i];
+                int moveX = now[1] + dx[i];
+
+                if (0 <= moveY && moveY < n && 0 <= moveX && moveX < n && !open[moveY][moveX] && !visited[moveY][moveX]) {
+                    int gap = Math.abs(map[now[0]][now[1]] - map[moveY][moveX]);
+                    if (l <= gap && gap <= r) {
+                        q.offer(new int[]{moveY, moveX});
+                        open[moveY][moveX] = true;
+                        sum += map[moveY][moveX];
+                        visited[moveY][moveX] = true;
+                        count++;
+                    }
                 }
             }
         }
-        changeMap(change, sum);
-    }
 
-    private static boolean validation(int moveY, int moveX) {
-        return !(0 <= moveY && moveY < N && 0 <= moveX && moveX < N);
-    }
+        if (count == 1) {
+            return;
+        }
 
-    private static void changeMap(List<int[]> list, int sum) {
-        int changeValue = sum/list.size();
-        for (int[] l : list) {
-            map[l[0]][l[1]] = changeValue;
+        change = true;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (open[i][j]) {
+                    map[i][j] = sum / count;
+                }
+            }
         }
     }
 }
