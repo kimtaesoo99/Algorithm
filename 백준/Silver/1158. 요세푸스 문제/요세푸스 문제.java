bf3388b398568c106e94ea56ae3155cc20cfd8c2
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-02-05 14:05:25 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 524 ms, Memory: 292368 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		String[] str = br.readLine().split(" ");
+		int n = Integer.parseInt(str[0]);
+		int k = Integer.parseInt(str[1]);
+
+		Queue<Integer> q = new LinkedList<>();
+
+		for (int i = 1; i <= n; i++) {
+			q.offer(i);
+		}
+
+		sb.append("<");
+
+		while (!q.isEmpty()) {
+			for (int i = 0; i < k - 1; i++) {
+				Integer now = q.poll();
+				q.offer(now);
+			}
+			sb.append(q.poll());
+			if (!q.isEmpty()) {
+				sb.append(", ");
+			}
+		}
+		sb.append(">");
+
+		System.out.println(sb);
+	}
+}
