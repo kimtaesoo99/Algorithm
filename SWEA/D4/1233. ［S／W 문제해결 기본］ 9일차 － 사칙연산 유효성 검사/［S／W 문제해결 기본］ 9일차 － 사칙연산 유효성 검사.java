a1bba851fc6bc639fbe85888bf742ed9a0c764b2
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-02-06 14:06:32 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 129 ms, Memory: 21,516 KB -BaekjoonHub

---
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	private static final List<String> operation = Arrays.asList("/", "*", "+", "-");
+
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			int n = Integer.parseInt(br.readLine());
+
+			Node[] nodes = new Node[n + 1];
+			
+			for (int i = 1; i <= n; i++) {
+				st = new StringTokenizer(br.readLine());
+				int num = Integer.parseInt(st.nextToken());
+				String value = st.nextToken();
+				int left = 0;
+				int right = 0;
+				if (st.hasMoreTokens()) {
+					left = Integer.parseInt(st.nextToken());
+				}
+				if (st.hasMoreElements()) {
+					right = Integer.parseInt(st.nextToken());
+				}
+
+				nodes[i] = new Node(num, value, left, right);
+			}
+
+			sb.append("#").append(test_case).append(" ").append(check(nodes) ? 1 : 0).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	private static boolean check(Node[] nodes) {
+		for (int i = 1; i < nodes.length; i++) {
+			if (nodes[i].left == 0 && nodes[i].right == 0) {
+				if(operation.contains(nodes[i].value)) {
+					return false;
+				}
+			} else {
+				if(!operation.contains(nodes[i].value)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
+
+class Node {
+	int num;
+	String value;
+	int left;
+	int right;
+
+	public Node(int num, String value, int left, int right) {
+		super();
+		this.num = num;
+		this.value = value;
+		this.left = left;
+		this.right = right;
+	}
+}
