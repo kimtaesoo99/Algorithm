83835558c6f2b7b0d24f889396a5d229c67aaabe
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-02-07 13:44:58 +0900

    [Silver I] Title: 절댓값 힙, Time: 280 ms, Memory: 23308 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,24 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.PriorityQueue;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        StringBuilder sb = new StringBuilder();
-        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
-            int abs1 = Math.abs(o1);
-            int abs2 = Math.abs(o2);
-            if(abs1 == abs2) return o1-o2;
-            return abs1 - abs2;
-        });
-        for(int i = 0 ; i < n; i++){
-            int val = sc.nextInt();
-            if(val == 0){
-                if (queue.isEmpty()) sb.append(0).append("\n");
-                else sb.append(queue.poll()).append("\n");
-            }else queue.add(val);
-        }
-        System.out.println(sb);
-    }
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				int a1 = Math.abs(o1);
+				int a2 = Math.abs(o2);
+				if (a1 == a2) {
+					return o1 - o2;
+				}
+				return a1 - a2;
+			}
+		});
+
+		for (int i = 0; i < n; i++) {
+			int value = Integer.parseInt(br.readLine());
+			if (value == 0) {
+				sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
+			} else {
+				pq.offer(value);
+			}
+		}
+
+		System.out.println(sb);
+	}
 }
