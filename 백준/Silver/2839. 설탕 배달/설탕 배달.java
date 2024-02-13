3290089f10a158aa6b7af45febf58d0e79d50478
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-02-13 16:06:21 +0900

    [Silver IV] Title: 설탕 배달, Time: 76 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,23 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+class Main {
 
-public class Main {
-	   public static void main(String[] args) {	
-		   
-		   Scanner sc = new Scanner(System.in);
-		   int num = sc.nextInt();
-		   int count = 0;
-		   while(true) {
-			   if(num%5 ==0) {
-				   System.out.println(num/5+count);
-				   break;
-			   }
-			   else if(num<0) {
-				   System.out.println(-1);
-				   break;
-			   }
-			   num=num-3;
-			   count++;
-		   }
-	  }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int[] dp = new int[5001];
+		dp[3] = 1;
+		dp[5] = 1;
+
+		for (int i = 6; i <= 5000; i++) {
+			int i3 = dp[i - 3];
+			int i5 = dp[i - 5];
+			if (i3 == 0 && i5 == 0) {
+				continue;
+			} else if (i3 == 0) {
+				dp[i] = i5 + 1;
+			} else if (i5 == 0) {
+				dp[i] = i3 + 1;
+			} else {
+				dp[i] = Math.min(i3, i5) + 1;
+			}
+		}
+
+		int result = dp[Integer.parseInt(br.readLine())];
+		System.out.println(result == 0 ? -1 : result);
+	}
 }
