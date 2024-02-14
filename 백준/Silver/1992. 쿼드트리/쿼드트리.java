4618fb65c5d57c4b1e0385fd77ff2573186ad460
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-02-14 12:17:52 +0900

    [Silver I] Title: 쿼드트리, Time: 116 ms, Memory: 12472 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,48 +1,58 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main{
-    static int[][] map;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        map = new int[n][n];
-        for (int i = 0; i < n; i++){
-            String[] info = br.readLine().split("");
-            for (int j = 0 ; j < n; j++){
-                map[i][j] = Integer.parseInt(info[j]);
-            }
-        }
+class Main {
 
-        partition(0,0,n);
-        System.out.println(sb);
-    }
-    public static void partition(int y, int x, int size){
-        if (isSameNum(y,x,size)){
-            int num = map[y][x];
-            if (num ==0)sb.append("0");
-            else sb.append("1");
-            return;
-        }
-        int newSize = size/2;
-        sb.append("(");
-        partition(y,x,newSize);
-        partition(y,x+newSize,newSize);
-        partition(y+newSize,x,newSize);
-        partition(y+newSize,x+newSize,newSize);
-        sb.append(")");
-    }
+	private static StringBuilder sb;
+	private static int[][] arr;
+	private static int now;
 
-    public static boolean isSameNum(int y, int x, int size){
-        int num = map[y][x];
-        for (int i = y; i < y+size; i++){
-            for (int j = x; j < x+size; j++){
-                if (map[i][j] != num)return false;
-            }
-        }
-        return true;
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+
+		arr = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			String[] line = br.readLine().split("");
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = Integer.parseInt(line[j]);
+			}
+		}
+
+		back(0, 0, n);
+		System.out.println(sb);
+	}
+
+	private static void back(int startY, int startX, int size) {
+		if (isSameTree(startY, startX, size)) {
+			sb.append(now);
+			return;
+		}
+
+		int nextSize = size / 2;
+		sb.append("(");
+		back(startY, startX, nextSize);
+		back(startY, startX + nextSize, nextSize);
+		back(startY + nextSize, startX, nextSize);
+		back(startY + nextSize, startX + nextSize, nextSize);
+		sb.append(")");
+	}
+
+	private static boolean isSameTree(int startY, int startX, int size) {
+		int pre = arr[startY][startX];
+
+		for (int i = startY; i < startY + size; i++) {
+			for (int j = startX; j < startX + size; j++) {
+				if (arr[i][j] != pre) {
+					return false;
+				}
+			}
+		}
+
+		now = arr[startY][startX];
+
+		return true;
+	}
 }
-
