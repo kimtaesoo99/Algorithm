669f236493953e9a177d419832580a35465201fa
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-02-18 08:22:51 +0900

    [Gold V] Title: 토마토, Time: 568 ms, Memory: 123960 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,29 +1,33 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int M;
-    static int[][] map;
-    static int[][] result;
-    static boolean[][] visited;
-    static int[] dy = {-1, 1, 0, 0};
-    static int[] dx = {0, 0, 1, -1};
+
+    private static int[] dy = {-1, 1, 0, 0};
+    private static int[] dx = {0, 0, 1, -1};
+    private static int N;
+    private static int M;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        map = new int[M][N];
-        result = new int[M][N];
-        visited = new boolean[M][N];
+
+        int[][] map = new int[M][N];
+        int[][] result = new int[M][N];
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == -1) result[i][j] = -1;
+                if (map[i][j] == -1) {
+                    result[i][j] = -1;
+                }
             }
         }
 
@@ -44,31 +48,34 @@ public class Main {
             for (int i = 0; i < 4; i++) {
                 int moveY = move[0] + dy[i];
                 int moveX = move[1] + dx[i];
-                if (0 <= moveY && moveY < M && 0 <= moveX && moveX < N) {
-                    if (map[moveY][moveX] == 0 && result[moveY][moveX] == 0) {
-                        result[moveY][moveX] = result[move[0]][move[1]] + 1;
-                        q.offer(new int[]{moveY, moveX});
-                    }
+                if (inMap(moveY, moveX) && (map[moveY][moveX] == 0 && result[moveY][moveX] == 0)) {
+                    result[moveY][moveX] = result[move[0]][move[1]] + 1;
+                    q.offer(new int[]{moveY, moveX});
                 }
             }
         }
+
         int max = 0;
         boolean pass = false;
-        for (int i = 0; i < M; i++) {
+
+        loop : for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
-                if (result[i][j] == 0){
+                if (result[i][j] == 0) {
                     pass = true;
-                    break;
+                    break loop;
                 }
                 max = Math.max(max, result[i][j]);
             }
         }
-        
-        if (pass)
-            System.out.println(-1);
-        else {
-            System.out.println(max-1);
-        }
 
+        if (pass) {
+            System.out.println(-1);
+        } else {
+            System.out.println(max - 1);
+        }
+    }
+
+    private static boolean inMap(int moveY, int moveX) {
+        return 0 <= moveY && moveY < M && 0 <= moveX && moveX < N;
     }
 }
