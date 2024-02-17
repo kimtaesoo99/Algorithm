5e5976b255b653a0fad103e8ee2c88af1cd21f38
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-02-18 08:07:18 +0900

    [Silver II] Title: 촌수계산, Time: 76 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,52 +1,67 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n + 1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] parents = new int[n + 1];
         int[] count = new int[n + 1];
         boolean[] visited = new boolean[n + 1];
-        int find1 = sc.nextInt();
-        int find2 = sc.nextInt();
 
-        int m = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int find1 = Integer.parseInt(st.nextToken());
+        int find2 = Integer.parseInt(st.nextToken());
+
+        int m = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < m; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            arr[b] = a;
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            parents[b] = a;
         }
-        int sum = 0;
+
+        int sum;
 
         int distance = 1;
         visited[find1] = true;
-        while (true){
-            int parent = arr[find1];
-            if (parent==0)break;
+
+        while (true) {
+            int parent = parents[find1];
+            if (parent == 0) {
+                break;
+            }
             visited[parent] = true;
             count[parent] = distance;
             distance++;
             find1 = parent;
         }
         distance = 1;
+
         if (visited[find2]) {
             System.out.println(count[find2]);
             return;
         }
-        while (true){
-            int parent = arr[find2];
-            if (parent==0){
-                sum =-1;
+
+        while (true) {
+            int parent = parents[find2];
+            if (parent == 0) {
+                sum = -1;
                 break;
             }
-            if (visited[parent]){
+            if (visited[parent]) {
                 sum = count[parent] + distance;
                 break;
             }
             distance++;
             find2 = parent;
         }
+
         System.out.println(sum);
     }
 }
