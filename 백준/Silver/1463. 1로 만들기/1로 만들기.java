12790d83ebfa80b9375802f0bd3ba64f91e41ed9
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-02-27 12:06:04 +0900

    [Silver III] Title: 1로 만들기, Time: 108 ms, Memory: 15496 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
 
@@ -12,34 +10,13 @@ public class Main {
 		int n = Integer.parseInt(br.readLine());
 
 		int[] dp = new int[n + 1];
-		Arrays.fill(dp, 1000000);
-		dp[n] = 0;
-		
-		Queue<Integer> q = new LinkedList<>();
-		q.add(n);
-
-		while (true) {
-			int now = q.poll();
-
-			if (dp[1] != 1000000) {
-				break;
-			}
-
-			if (now % 3 == 0) {
-				dp[now / 3] = Math.min(dp[now / 3], dp[now] + 1);
-				q.offer(now / 3);
-			}
-
-			if (now % 2 == 0) {
-				dp[now / 2] = Math.min(dp[now / 2], dp[now] + 1);
-				q.offer(now / 2);
-			}
-
-			dp[now - 1] = Math.min(dp[now - 1], dp[now] + 1);
-			if (now - 1 > 0) {
-				q.offer(now - 1);
-			}
+		Arrays.fill(dp, Integer.MAX_VALUE);
+		dp[1] = 0;
+		for (int i = 2; i <= n; i++) {
+			dp[i] = dp[i - 1] + 1;
+			if (i % 2 == 0) dp[i] = Math.min(1 + dp[i / 2], dp[i]);
+			if (i % 3 == 0) dp[i] = Math.min(1 + dp[i / 3], dp[i]);
 		}
-		System.out.println(dp[1]);
+		System.out.println(dp[n]);
 	}
 }
