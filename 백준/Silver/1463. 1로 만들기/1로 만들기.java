66de9d84026df8d5166f7aa08302108a80e090be
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-02-27 12:01:32 +0900

    [Silver III] Title: 1로 만들기, Time: 232 ms, Memory: 112724 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,24 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class Main {
 
-public class Main{
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 
-        int x = Integer.parseInt(br.readLine());
+		int[] dp = new int[n + 1];
+		Arrays.fill(dp, 1000000);
+		dp[n] = 0;
+		
+		Queue<Integer> q = new LinkedList<>();
+		q.add(n);
 
-        int dp[] = new int[x + 1];
+		while (true) {
+			int now = q.poll();
 
-        dp[0] = dp[1] = 0;
+			if (dp[1] != 1000000) {
+				break;
+			}
 
-        for (int i = 2; i <= x; i++) {
-            dp[i] = dp[i - 1] + 1;
-            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1); 
-        }
+			if (now % 3 == 0) {
+				dp[now / 3] = Math.min(dp[now / 3], dp[now] + 1);
+				q.offer(now / 3);
+			}
 
-        System.out.println(dp[x]);
-    }
+			if (now % 2 == 0) {
+				dp[now / 2] = Math.min(dp[now / 2], dp[now] + 1);
+				q.offer(now / 2);
+			}
+
+			dp[now - 1] = Math.min(dp[now - 1], dp[now] + 1);
+			if (now - 1 > 0) {
+				q.offer(now - 1);
+			}
+		}
+		System.out.println(dp[1]);
+	}
 }
