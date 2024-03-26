901597ce8d7b34f2acbce7095bb980f25109bddc
Taesoo Kim <107785279+kimtaesoo99@users.noreply.github.com>
2024-03-26 13:51:04 +0900

    [Gold V] Title: 평범한 배낭, Time: 132 ms, Memory: 13364 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,21 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int[][] dp = new int[N+1][M + 1];
+public class Main {
 
-        for (int i = 1; i<=N; i++){
-            int w = sc.nextInt();
-            int v = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-            for (int j = 1; j<=M; j++){
-                if (w>j)dp[i][j] = dp[i-1][j];
-                else dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-w] + v);
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[K + 1];
+
+        for (int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            int W = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+
+            for (int j = K; j >= W; j--){
+                dp[j] = Math.max(dp[j], dp[j - W] + V);
             }
         }
-        System.out.println(dp[N][M]);
+
+        System.out.println(dp[K]);
     }
 }
